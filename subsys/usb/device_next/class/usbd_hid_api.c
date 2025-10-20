zephyrproject-rtos/zephyr

usb: device_next: hid: allow to set polling period at runtime
Allow to set input or output report polling period at runtime.

Signed-off-by: Johann Fischer <johann.fischer@nordicsemi.no>

subsys/usb/device_next/class/usbd_hid_api.c
@@ -34,6 +34,28 @@ int hid_device_register(const struct device *dev,
 	return api->dev_register(dev, rdesc, rsize, ops);
 }
 
+int hid_device_set_in_polling(const struct device *dev, const unsigned int period)
+{
+	const struct hid_device_driver_api *const api = dev->api;
+
+	if (IS_ENABLED(CONFIG_USBD_HID_SET_POLLING_PERIOD)) {
+		return api->set_in_polling(dev, period);
+	}
+
+	return -ENOTSUP;
+}
+
+int hid_device_set_out_polling(const struct device *dev, const unsigned int period)
+{
+	const struct hid_device_driver_api *const api = dev->api;
+
+	if (IS_ENABLED(CONFIG_USBD_HID_SET_POLLING_PERIOD)) {
+		return api->set_out_polling(dev, period);
+	}
+
+	return -ENOTSUP;
+}
+
 /* Legacy HID API wrapper below */
 
 struct legacy_wrapper {

subsys/usb/device_next/class/usbd_hid_internal.h
@@ -20,4 +20,6 @@ struct hid_device_driver_api {
 	int (*dev_register)(const struct device *dev,
 			    const uint8_t *const rdesc, const uint16_t rsize,
 			    const struct hid_device_ops *const ops);
+	int (*set_in_polling)(const struct device *dev, const unsigned int value);
+	int (*set_out_polling)(const struct device *dev, const unsigned int value);
 };

Infineon/zephyr-aurix

subsys/usb/device_next/class/usbd_hid_api.c
@@ -34,6 +34,28 @@ int hid_device_register(const struct device *dev,
 	return api->dev_register(dev, rdesc, rsize, ops);
 }
 
+int hid_device_set_in_polling(const struct device *dev, const unsigned int period_us)
+{
+	const struct hid_device_driver_api *const api = dev->api;
+
+	if (IS_ENABLED(CONFIG_USBD_HID_SET_POLLING_PERIOD)) {
+		return api->set_in_polling(dev, period_us);
+	}
+
+	return -ENOTSUP;
+}
+
+int hid_device_set_out_polling(const struct device *dev, const unsigned int period_us)
+{
+	const struct hid_device_driver_api *const api = dev->api;
+
+	if (IS_ENABLED(CONFIG_USBD_HID_SET_POLLING_PERIOD)) {
+		return api->set_out_polling(dev, period_us);
+	}
+
+	return -ENOTSUP;
+}
+
 /* Legacy HID API wrapper below */
 
 struct legacy_wrapper {

subsys/usb/device_next/class/usbd_hid_internal.h
@@ -20,4 +20,6 @@ struct hid_device_driver_api {
 	int (*dev_register)(const struct device *dev,
 			    const uint8_t *const rdesc, const uint16_t rsize,
 			    const struct hid_device_ops *const ops);
+	int (*set_in_polling)(const struct device *dev, const unsigned int period_us);
+	int (*set_out_polling)(const struct device *dev, const unsigned int period_us);
 };