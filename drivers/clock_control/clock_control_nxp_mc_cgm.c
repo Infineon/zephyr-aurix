zephyrproject-rtos/zephyr

drivers: clock_control: add nxp_mc_cgm clock driver
- add clock_init function to initialize clock sources according
  devicetree settings
- finish basic clock api function

Signed-off-by: Lucien Zhao <lucien.zhao@nxp.com>

drivers/clock_control/clock_control_nxp_mc_cgm.c
@@ -1,0 +1,317 @@
+/*
+ * Copyright 2025 NXP
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#define DT_DRV_COMPAT nxp_mc_cgm
+
+#include <errno.h>
+#include <zephyr/drivers/clock_control/nxp_clock_control.h>
+#include <fsl_clock.h>
+
+#define LOG_LEVEL CONFIG_CLOCK_CONTROL_LOG_LEVEL
+#include <zephyr/logging/log.h>
+LOG_MODULE_REGISTER(clock_control);
+
+#if DT_NODE_HAS_COMPAT_STATUS(DT_NODELABEL(fxosc), nxp_fxosc, okay)
+const fxosc_config_t g_fxoscConfig_BOARD_BootClockRUN = {.freqHz = NXP_FXOSC_FREQ,
+							 .workMode = NXP_FXOSC_WORKMODE,
+							 .startupDelay = NXP_FXOSC_DELAY,
+							 .overdriveProtect = NXP_FXOSC_OVERDRIVE};
+#endif
+
+#if DT_NODE_HAS_COMPAT_STATUS(DT_NODELABEL(pll), nxp_plldig, okay)
+const pll_config_t g_pllConfig_BOARD_BootClockRUN = {
+	.workMode = NXP_PLL_WORKMODE,
+	.preDiv = NXP_PLL_PREDIV, /* PLL input clock predivider: 2 */
+	.postDiv = NXP_PLL_POSTDIV,
+	.multiplier = NXP_PLL_MULTIPLIER,
+	.fracLoopDiv = NXP_PLL_FRACLOOPDIV,
+	.stepSize = NXP_PLL_STEPSIZE,
+	.stepNum = NXP_PLL_STEPNUM,
+	.accuracy = NXP_PLL_ACCURACY,
+	.outDiv = NXP_PLL_OUTDIV_POINTER};
+#endif
+
+#if DT_NODE_HAS_COMPAT_STATUS(DT_NODELABEL(mc_cgm), nxp_mc_cgm, okay)
+const clock_pcfs_config_t g_pcfsConfig_BOARD_BootClockRUN = {.maxAllowableIDDchange =
+								     NXP_PLL_MAXIDOCHANGE,
+							     .stepDuration = NXP_PLL_STEPDURATION,
+							     .clkSrcFreq = NXP_PLL_CLKSRCFREQ};
+#endif
+
+static int mc_cgm_clock_control_on(const struct device *dev, clock_control_subsys_t sub_system)
+{
+#if defined(CONFIG_CAN_MCUX_FLEXCAN)
+	switch ((uint32_t)sub_system) {
+	case MCUX_FLEXCAN0_CLK:
+		CLOCK_EnableClock(kCLOCK_Flexcan0);
+		break;
+	case MCUX_FLEXCAN1_CLK:
+		CLOCK_EnableClock(kCLOCK_Flexcan1);
+		break;
+	case MCUX_FLEXCAN2_CLK:
+		CLOCK_EnableClock(kCLOCK_Flexcan2);
+		break;
+	case MCUX_FLEXCAN3_CLK:
+		CLOCK_EnableClock(kCLOCK_Flexcan3);
+		break;
+	case MCUX_FLEXCAN4_CLK:
+		CLOCK_EnableClock(kCLOCK_Flexcan4);
+		break;
+	case MCUX_FLEXCAN5_CLK:
+		CLOCK_EnableClock(kCLOCK_Flexcan5);
+		break;
+	default:
+		break;
+	}
+#endif /* defined(CONFIG_CAN_MCUX_MCAN) */
+
+#if defined(CONFIG_UART_MCUX_LPUART)
+	switch ((uint32_t)sub_system) {
+	case MCUX_LPUART0_CLK:
+		CLOCK_EnableClock(kCLOCK_Lpuart0);
+		break;
+	case MCUX_LPUART1_CLK:
+		CLOCK_EnableClock(kCLOCK_Lpuart1);
+		break;
+	case MCUX_LPUART2_CLK:
+		CLOCK_EnableClock(kCLOCK_Lpuart2);
+		break;
+	case MCUX_LPUART3_CLK:
+		CLOCK_EnableClock(kCLOCK_Lpuart3);
+		break;
+	case MCUX_LPUART4_CLK:
+		CLOCK_EnableClock(kCLOCK_Lpuart4);
+		break;
+	case MCUX_LPUART5_CLK:
+		CLOCK_EnableClock(kCLOCK_Lpuart5);
+		break;
+	case MCUX_LPUART6_CLK:
+		CLOCK_EnableClock(kCLOCK_Lpuart6);
+		break;
+	case MCUX_LPUART7_CLK:
+		CLOCK_EnableClock(kCLOCK_Lpuart7);
+		break;
+	case MCUX_LPUART8_CLK:
+		CLOCK_EnableClock(kCLOCK_Lpuart8);
+		break;
+	case MCUX_LPUART9_CLK:
+		CLOCK_EnableClock(kCLOCK_Lpuart9);
+		break;
+	case MCUX_LPUART10_CLK:
+		CLOCK_EnableClock(kCLOCK_Lpuart10);
+		break;
+	case MCUX_LPUART11_CLK:
+		CLOCK_EnableClock(kCLOCK_Lpuart11);
+		break;
+	case MCUX_LPUART12_CLK:
+		CLOCK_EnableClock(kCLOCK_Lpuart12);
+		break;
+	case MCUX_LPUART13_CLK:
+		CLOCK_EnableClock(kCLOCK_Lpuart13);
+		break;
+	case MCUX_LPUART14_CLK:
+		CLOCK_EnableClock(kCLOCK_Lpuart14);
+		break;
+	case MCUX_LPUART15_CLK:
+		CLOCK_EnableClock(kCLOCK_Lpuart15);
+		break;
+	default:
+		break;
+	}
+#endif /* defined(CONFIG_UART_MCUX_LPUART) */
+
+#if defined(CONFIG_SPI_NXP_LPSPI)
+	switch ((uint32_t)sub_system) {
+	case MCUX_LPSPI0_CLK:
+		CLOCK_EnableClock(kCLOCK_Lpspi0);
+		break;
+	case MCUX_LPSPI1_CLK:
+		CLOCK_EnableClock(kCLOCK_Lpspi1);
+		break;
+	case MCUX_LPSPI2_CLK:
+		CLOCK_EnableClock(kCLOCK_Lpspi2);
+		break;
+	case MCUX_LPSPI3_CLK:
+		CLOCK_EnableClock(kCLOCK_Lpspi3);
+		break;
+	case MCUX_LPSPI4_CLK:
+		CLOCK_EnableClock(kCLOCK_Lpspi4);
+		break;
+	case MCUX_LPSPI5_CLK:
+		CLOCK_EnableClock(kCLOCK_Lpspi5);
+		break;
+	default:
+		break;
+	}
+#endif /* defined(CONFIG_SPI_NXP_LPSPI) */
+
+#if defined(CONFIG_I2C_MCUX_LPI2C)
+	switch ((uint32_t)sub_system) {
+	case MCUX_LPI2C0_CLK:
+		CLOCK_EnableClock(kCLOCK_Lpi2c0);
+		break;
+	case MCUX_LPI2C1_CLK:
+		CLOCK_EnableClock(kCLOCK_Lpi2c1);
+		break;
+	default:
+		break;
+	}
+#endif /* defined(CONFIG_I2C_MCUX_LPI2C) */
+
+	return 0;
+}
+
+static int mc_cgm_clock_control_off(const struct device *dev, clock_control_subsys_t sub_system)
+{
+	return 0;
+}
+
+static int mc_cgm_get_subsys_rate(const struct device *dev, clock_control_subsys_t sub_system,
+				  uint32_t *rate)
+{
+	uint32_t clock_name = (uint32_t)sub_system;
+
+	switch (clock_name) {
+#if defined(CONFIG_UART_MCUX_LPUART)
+	case MCUX_LPUART0_CLK:
+	case MCUX_LPUART8_CLK:
+		*rate = CLOCK_GetAipsPlatClkFreq();
+		break;
+	case MCUX_LPUART1_CLK:
+	case MCUX_LPUART2_CLK:
+	case MCUX_LPUART3_CLK:
+	case MCUX_LPUART4_CLK:
+	case MCUX_LPUART5_CLK:
+	case MCUX_LPUART6_CLK:
+	case MCUX_LPUART7_CLK:
+	case MCUX_LPUART9_CLK:
+	case MCUX_LPUART10_CLK:
+	case MCUX_LPUART11_CLK:
+	case MCUX_LPUART12_CLK:
+	case MCUX_LPUART13_CLK:
+	case MCUX_LPUART14_CLK:
+	case MCUX_LPUART15_CLK:
+		*rate = CLOCK_GetAipsSlowClkFreq();
+		break;
+#endif /* defined(CONFIG_UART_MCUX_LPUART) */
+
+#if defined(CONFIG_SPI_NXP_LPSPI)
+	case MCUX_LPSPI0_CLK:
+		*rate = CLOCK_GetAipsPlatClkFreq();
+		break;
+	case MCUX_LPSPI1_CLK:
+	case MCUX_LPSPI2_CLK:
+	case MCUX_LPSPI3_CLK:
+	case MCUX_LPSPI4_CLK:
+	case MCUX_LPSPI5_CLK:
+		*rate = CLOCK_GetAipsSlowClkFreq();
+		break;
+#endif /* defined(CONFIG_SPI_NXP_LPSPI) */
+
+#if defined(CONFIG_I2C_MCUX_LPI2C)
+	case MCUX_LPI2C0_CLK:
+	case MCUX_LPI2C1_CLK:
+		*rate = CLOCK_GetAipsSlowClkFreq();
+		break;
+#endif /* defined(CONFIG_I2C_MCUX_LPI2C) */
+
+#if defined(CONFIG_CAN_MCUX_FLEXCAN)
+	case MCUX_FLEXCAN0_CLK:
+		*rate = CLOCK_GetFlexcanPeClkFreq(0);
+		break;
+	case MCUX_FLEXCAN1_CLK:
+		*rate = CLOCK_GetFlexcanPeClkFreq(1);
+		break;
+	case MCUX_FLEXCAN2_CLK:
+		*rate = CLOCK_GetFlexcanPeClkFreq(2);
+		break;
+	case MCUX_FLEXCAN3_CLK:
+		*rate = CLOCK_GetFlexcanPeClkFreq(3);
+		break;
+	case MCUX_FLEXCAN4_CLK:
+		*rate = CLOCK_GetFlexcanPeClkFreq(4);
+		break;
+	case MCUX_FLEXCAN5_CLK:
+		*rate = CLOCK_GetFlexcanPeClkFreq(5);
+		break;
+#endif /* defined(CONFIG_CAN_MCUX_FLEXCAN) */
+	}
+	return 0;
+}
+
+static int mc_cgm_init(const struct device *dev)
+{
+#if defined(FSL_FEATURE_PMC_HAS_LAST_MILE_REGULATOR) && (FSL_FEATURE_PMC_HAS_LAST_MILE_REGULATOR)
+	/* Enables PMC last mile regulator before enable PLL.  */
+	if ((PMC->LVSC & PMC_LVSC_LVD15S_MASK) != 0U) {
+		/* External bipolar junction transistor is connected between external voltage and
+		 * V15 input pin.
+		 */
+		PMC->CONFIG |= PMC_CONFIG_LMBCTLEN_MASK;
+	}
+	while ((PMC->LVSC & PMC_LVSC_LVD15S_MASK) != 0U) {
+	}
+	PMC->CONFIG |= PMC_CONFIG_LMEN_MASK;
+	while ((PMC->CONFIG & PMC_CONFIG_LMSTAT_MASK) == 0u) {
+	}
+#endif /* FSL_FEATURE_PMC_HAS_LAST_MILE_REGULATOR */
+
+#if DT_NODE_HAS_COMPAT_STATUS(DT_NODELABEL(firc), nxp_firc, okay)
+	/* Switch the FIRC_DIV_SEL to the desired diveder. */
+	CLOCK_SetFircDiv(NXP_FIRC_DIV);
+	/* Disable FIRC in standby mode. */
+	CLOCK_DisableFircInStandbyMode();
+#endif
+
+#if DT_NODE_HAS_COMPAT_STATUS(DT_NODELABEL(sirc), nxp_sirc, okay)
+	/* Disable SIRC in standby mode. */
+	CLOCK_DisableSircInStandbyMode();
+#endif
+
+#if DT_NODE_HAS_COMPAT_STATUS(DT_NODELABEL(fxosc), nxp_fxosc, okay)
+	/* Enable FXOSC. */
+	CLOCK_InitFxosc(&g_fxoscConfig_BOARD_BootClockRUN);
+#endif
+
+#if DT_NODE_HAS_COMPAT_STATUS(DT_NODELABEL(pll), nxp_plldig, okay)
+	/* Enable PLL. */
+	CLOCK_InitPll(&g_pllConfig_BOARD_BootClockRUN);
+#endif
+
+#if DT_NODE_HAS_COMPAT_STATUS(DT_NODELABEL(mc_cgm), nxp_mc_cgm, okay)
+	CLOCK_SelectSafeClock(kFIRC_CLK_to_MUX0);
+	/* Configure MUX_0_CSC dividers */
+	CLOCK_SetClkMux0DivTriggerType(KCLOCK_CommonTriggerUpdate);
+	CLOCK_SetClkDiv(kCLOCK_DivCoreClk, NXP_PLL_MUX_0_DC_0_DIV);
+	CLOCK_SetClkDiv(kCLOCK_DivAipsPlatClk, NXP_PLL_MUX_0_DC_1_DIV);
+	CLOCK_SetClkDiv(kCLOCK_DivAipsSlowClk, NXP_PLL_MUX_0_DC_2_DIV);
+	CLOCK_SetClkDiv(kCLOCK_DivHseClk, NXP_PLL_MUX_0_DC_3_DIV);
+	CLOCK_SetClkDiv(kCLOCK_DivDcmClk, NXP_PLL_MUX_0_DC_4_DIV);
+#ifdef MC_CGM_MUX_0_DC_5_DIV_MASK
+	CLOCK_SetClkDiv(kCLOCK_DivLbistClk, NXP_PLL_MUX_0_DC_5_DIV);
+#endif
+#ifdef MC_CGM_MUX_0_DC_6_DIV_MASK
+	CLOCK_SetClkDiv(kCLOCK_DivQspiClk, NXP_PLL_MUX_0_DC_6_DIV);
+#endif
+	CLOCK_CommonTriggerClkMux0DivUpdate();
+	CLOCK_ProgressiveClockFrequencySwitch(kPLL_PHI0_CLK_to_MUX0,
+					      &g_pcfsConfig_BOARD_BootClockRUN);
+#endif
+
+	/* Set SystemCoreClock variable. */
+	SystemCoreClockUpdate();
+
+	return 0;
+}
+
+static DEVICE_API(clock_control, mcux_mcxe31x_clock_api) = {
+	.on = mc_cgm_clock_control_on,
+	.off = mc_cgm_clock_control_off,
+	.get_rate = mc_cgm_get_subsys_rate,
+};
+
+DEVICE_DT_INST_DEFINE(0, mc_cgm_init, NULL, NULL, NULL, PRE_KERNEL_1,
+		      CONFIG_CLOCK_CONTROL_INIT_PRIORITY, &mcux_mcxe31x_clock_api);

include/zephyr/dt-bindings/clock/nxp_mc_cgm.h
@@ -1,0 +1,101 @@
+/*
+ * Copyright 2025 NXP
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef ZEPHYR_INCLUDE_DT_BINDINGS_CLOCK_NXP_MC_CGM_H_
+#define ZEPHYR_INCLUDE_DT_BINDINGS_CLOCK_NXP_MC_CGM_H_
+
+/* Note- clock identifiers in this file must be unique,
+ * as the driver uses them in a switch case
+ */
+
+#define MCUX_MCXE31X_CLK_ID(high, low) ((high << 8) | (low))
+
+/* These IDs are used within SOC macros, and thus cannot be defined
+ * using the standard MCUX_MCXE31X_CLK_ID form
+ */
+/* --------------------- System layer clock --------------------- */
+#define MCUX_CORESYS_CLK  MCUX_MCXE31X_CLK_ID(0x00, 0x00)
+#define MCUX_AIPSPLAT_CLK MCUX_MCXE31X_CLK_ID(0x01, 0x00)
+#define MCUX_AIPSSLOW_CLK MCUX_MCXE31X_CLK_ID(0x02, 0x00)
+#define MCUX_HSE_CLK      MCUX_MCXE31X_CLK_ID(0x03, 0x00)
+#define MCUX_DCM_CLK      MCUX_MCXE31X_CLK_ID(0x04, 0x00)
+#define MCUX_LBIST_CLK    MCUX_MCXE31X_CLK_ID(0x05, 0x00)
+#define MCUX_QSPI_CLK     MCUX_MCXE31X_CLK_ID(0x06, 0x00)
+
+/* --------------------- MC_CGM clock --------------------- */
+#define MCUX_FIRC_CLK    MCUX_MCXE31X_CLK_ID(0x10, 0x00)
+#define MCUX_SIRC_CLK    MCUX_MCXE31X_CLK_ID(0x11, 0x00)
+#define MCUX_FXOSC_CLK   MCUX_MCXE31X_CLK_ID(0x12, 0x00)
+#define MCUX_SXOSC_CLK   MCUX_MCXE31X_CLK_ID(0x13, 0x00)
+#define MCUX_PLLPHI0_CLK MCUX_MCXE31X_CLK_ID(0x14, 0x00)
+#define MCUX_PLLPHI1_CLK MCUX_MCXE31X_CLK_ID(0x14, 0x01)
+
+/* --------------------- Peripheral clock --------------------- */
+#define MCUX_ADC0_CLK MCUX_MCXE31X_CLK_ID(0x20, 0x00)
+#define MCUX_ADC1_CLK MCUX_MCXE31X_CLK_ID(0x20, 0x01)
+#define MCUX_ADC2_CLK MCUX_MCXE31X_CLK_ID(0x20, 0x02)
+
+#define MCUX_BCTU_CLK MCUX_MCXE31X_CLK_ID(0x21, 0x00)
+
+#define MCUX_CMP0_CLK MCUX_MCXE31X_CLK_ID(0x22, 0x00)
+#define MCUX_CMP1_CLK MCUX_MCXE31X_CLK_ID(0x22, 0x01)
+#define MCUX_CMP2_CLK MCUX_MCXE31X_CLK_ID(0x22, 0x02)
+
+#define MCUX_EMIOS_CLK MCUX_MCXE31X_CLK_ID(0x23, 0x00)
+
+#define MCUX_FLEXCAN0_CLK MCUX_MCXE31X_CLK_ID(0x24, 0x00)
+#define MCUX_FLEXCAN1_CLK MCUX_MCXE31X_CLK_ID(0x24, 0x01)
+#define MCUX_FLEXCAN2_CLK MCUX_MCXE31X_CLK_ID(0x24, 0x02)
+#define MCUX_FLEXCAN3_CLK MCUX_MCXE31X_CLK_ID(0x24, 0x03)
+#define MCUX_FLEXCAN4_CLK MCUX_MCXE31X_CLK_ID(0x24, 0x04)
+#define MCUX_FLEXCAN5_CLK MCUX_MCXE31X_CLK_ID(0x24, 0x05)
+
+#define MCUX_FLEXIO_CLK MCUX_MCXE31X_CLK_ID(0x25, 0x00)
+
+#define MCUX_LPI2C0_CLK MCUX_MCXE31X_CLK_ID(0x26, 0x00)
+#define MCUX_LPI2C1_CLK MCUX_MCXE31X_CLK_ID(0x26, 0x01)
+
+#define MCUX_LPSPI0_CLK MCUX_MCXE31X_CLK_ID(0x27, 0x00)
+#define MCUX_LPSPI1_CLK MCUX_MCXE31X_CLK_ID(0x27, 0x01)
+#define MCUX_LPSPI2_CLK MCUX_MCXE31X_CLK_ID(0x27, 0x02)
+#define MCUX_LPSPI3_CLK MCUX_MCXE31X_CLK_ID(0x27, 0x03)
+#define MCUX_LPSPI4_CLK MCUX_MCXE31X_CLK_ID(0x27, 0x04)
+#define MCUX_LPSPI5_CLK MCUX_MCXE31X_CLK_ID(0x27, 0x05)
+
+#define MCUX_LPUART0_CLK  MCUX_MCXE31X_CLK_ID(0x28, 0x00)
+#define MCUX_LPUART1_CLK  MCUX_MCXE31X_CLK_ID(0x28, 0x01)
+#define MCUX_LPUART2_CLK  MCUX_MCXE31X_CLK_ID(0x28, 0x02)
+#define MCUX_LPUART3_CLK  MCUX_MCXE31X_CLK_ID(0x28, 0x03)
+#define MCUX_LPUART4_CLK  MCUX_MCXE31X_CLK_ID(0x28, 0x04)
+#define MCUX_LPUART5_CLK  MCUX_MCXE31X_CLK_ID(0x28, 0x05)
+#define MCUX_LPUART6_CLK  MCUX_MCXE31X_CLK_ID(0x28, 0x06)
+#define MCUX_LPUART7_CLK  MCUX_MCXE31X_CLK_ID(0x28, 0x07)
+#define MCUX_LPUART8_CLK  MCUX_MCXE31X_CLK_ID(0x28, 0x08)
+#define MCUX_LPUART9_CLK  MCUX_MCXE31X_CLK_ID(0x28, 0x09)
+#define MCUX_LPUART10_CLK MCUX_MCXE31X_CLK_ID(0x28, 0x0A)
+#define MCUX_LPUART11_CLK MCUX_MCXE31X_CLK_ID(0x28, 0x0B)
+#define MCUX_LPUART12_CLK MCUX_MCXE31X_CLK_ID(0x28, 0x0C)
+#define MCUX_LPUART13_CLK MCUX_MCXE31X_CLK_ID(0x28, 0x0D)
+#define MCUX_LPUART14_CLK MCUX_MCXE31X_CLK_ID(0x28, 0x0E)
+#define MCUX_LPUART15_CLK MCUX_MCXE31X_CLK_ID(0x28, 0x0F)
+
+#define MCUX_PIT0_CLK MCUX_MCXE31X_CLK_ID(0x29, 0x00)
+#define MCUX_PIT1_CLK MCUX_MCXE31X_CLK_ID(0x29, 0x01)
+#define MCUX_PIT2_CLK MCUX_MCXE31X_CLK_ID(0x29, 0x02)
+
+#define MCUX_SAI0_CLK MCUX_MCXE31X_CLK_ID(0x2A, 0x00)
+#define MCUX_SAI1_CLK MCUX_MCXE31X_CLK_ID(0x2A, 0x01)
+
+#define MCUX_STM0_CLK MCUX_MCXE31X_CLK_ID(0x2B, 0x00)
+#define MCUX_STM1_CLK MCUX_MCXE31X_CLK_ID(0x2B, 0x01)
+
+/* --------------------- Partition 2 clock --------------------- */
+#define MCUX_QSPISF_CLK MCUX_MCXE31X_CLK_ID(0x2C, 0x00)
+#define MCUX_EMACRX_CLK MCUX_MCXE31X_CLK_ID(0x2C, 0x01)
+#define MCUX_EMACTX_CLK MCUX_MCXE31X_CLK_ID(0x2C, 0x02)
+#define MCUX_EMACTS_CLK MCUX_MCXE31X_CLK_ID(0x2C, 0x03)
+
+#endif /* ZEPHYR_INCLUDE_DT_BINDINGS_CLOCK_NXP_MC_CGM_H_ */

Infineon/zephyr-aurix

drivers/clock_control/clock_control_nxp_mc_cgm.c
@@ -1,0 +1,315 @@
+/*
+ * Copyright 2025 NXP
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#define DT_DRV_COMPAT nxp_mc_cgm
+
+#include <errno.h>
+#include <zephyr/drivers/clock_control/nxp_clock_controller_sources.h>
+#include <zephyr/dt-bindings/clock/nxp_mc_cgm.h>
+#include <fsl_clock.h>
+
+#define LOG_LEVEL CONFIG_CLOCK_CONTROL_LOG_LEVEL
+#include <zephyr/logging/log.h>
+LOG_MODULE_REGISTER(clock_control);
+
+#if DT_NODE_HAS_COMPAT_STATUS(DT_NODELABEL(fxosc), nxp_fxosc, okay)
+const fxosc_config_t fxosc_config = {.freqHz = NXP_FXOSC_FREQ,
+				     .workMode = NXP_FXOSC_WORKMODE,
+				     .startupDelay = NXP_FXOSC_DELAY,
+				     .overdriveProtect = NXP_FXOSC_OVERDRIVE};
+#endif
+
+#if DT_NODE_HAS_COMPAT_STATUS(DT_NODELABEL(pll), nxp_plldig, okay)
+const pll_config_t pll_config = {.workMode = NXP_PLL_WORKMODE,
+				 .preDiv = NXP_PLL_PREDIV, /* PLL input clock predivider: 2 */
+				 .postDiv = NXP_PLL_POSTDIV,
+				 .multiplier = NXP_PLL_MULTIPLIER,
+				 .fracLoopDiv = NXP_PLL_FRACLOOPDIV,
+				 .stepSize = NXP_PLL_STEPSIZE,
+				 .stepNum = NXP_PLL_STEPNUM,
+				 .accuracy = NXP_PLL_ACCURACY,
+				 .outDiv = NXP_PLL_OUTDIV_POINTER};
+#endif
+
+#if DT_NODE_HAS_COMPAT_STATUS(DT_NODELABEL(mc_cgm), nxp_mc_cgm, okay)
+const clock_pcfs_config_t pcfs_config = {.maxAllowableIDDchange = NXP_PLL_MAXIDOCHANGE,
+					 .stepDuration = NXP_PLL_STEPDURATION,
+					 .clkSrcFreq = NXP_PLL_CLKSRCFREQ};
+#endif
+
+static int mc_cgm_clock_control_on(const struct device *dev, clock_control_subsys_t sub_system)
+{
+#if defined(CONFIG_CAN_MCUX_FLEXCAN)
+	switch ((uint32_t)sub_system) {
+	case MCUX_FLEXCAN0_CLK:
+		CLOCK_EnableClock(kCLOCK_Flexcan0);
+		break;
+	case MCUX_FLEXCAN1_CLK:
+		CLOCK_EnableClock(kCLOCK_Flexcan1);
+		break;
+	case MCUX_FLEXCAN2_CLK:
+		CLOCK_EnableClock(kCLOCK_Flexcan2);
+		break;
+	case MCUX_FLEXCAN3_CLK:
+		CLOCK_EnableClock(kCLOCK_Flexcan3);
+		break;
+	case MCUX_FLEXCAN4_CLK:
+		CLOCK_EnableClock(kCLOCK_Flexcan4);
+		break;
+	case MCUX_FLEXCAN5_CLK:
+		CLOCK_EnableClock(kCLOCK_Flexcan5);
+		break;
+	default:
+		break;
+	}
+#endif /* defined(CONFIG_CAN_MCUX_MCAN) */
+
+#if defined(CONFIG_UART_MCUX_LPUART)
+	switch ((uint32_t)sub_system) {
+	case MCUX_LPUART0_CLK:
+		CLOCK_EnableClock(kCLOCK_Lpuart0);
+		break;
+	case MCUX_LPUART1_CLK:
+		CLOCK_EnableClock(kCLOCK_Lpuart1);
+		break;
+	case MCUX_LPUART2_CLK:
+		CLOCK_EnableClock(kCLOCK_Lpuart2);
+		break;
+	case MCUX_LPUART3_CLK:
+		CLOCK_EnableClock(kCLOCK_Lpuart3);
+		break;
+	case MCUX_LPUART4_CLK:
+		CLOCK_EnableClock(kCLOCK_Lpuart4);
+		break;
+	case MCUX_LPUART5_CLK:
+		CLOCK_EnableClock(kCLOCK_Lpuart5);
+		break;
+	case MCUX_LPUART6_CLK:
+		CLOCK_EnableClock(kCLOCK_Lpuart6);
+		break;
+	case MCUX_LPUART7_CLK:
+		CLOCK_EnableClock(kCLOCK_Lpuart7);
+		break;
+	case MCUX_LPUART8_CLK:
+		CLOCK_EnableClock(kCLOCK_Lpuart8);
+		break;
+	case MCUX_LPUART9_CLK:
+		CLOCK_EnableClock(kCLOCK_Lpuart9);
+		break;
+	case MCUX_LPUART10_CLK:
+		CLOCK_EnableClock(kCLOCK_Lpuart10);
+		break;
+	case MCUX_LPUART11_CLK:
+		CLOCK_EnableClock(kCLOCK_Lpuart11);
+		break;
+	case MCUX_LPUART12_CLK:
+		CLOCK_EnableClock(kCLOCK_Lpuart12);
+		break;
+	case MCUX_LPUART13_CLK:
+		CLOCK_EnableClock(kCLOCK_Lpuart13);
+		break;
+	case MCUX_LPUART14_CLK:
+		CLOCK_EnableClock(kCLOCK_Lpuart14);
+		break;
+	case MCUX_LPUART15_CLK:
+		CLOCK_EnableClock(kCLOCK_Lpuart15);
+		break;
+	default:
+		break;
+	}
+#endif /* defined(CONFIG_UART_MCUX_LPUART) */
+
+#if defined(CONFIG_SPI_NXP_LPSPI)
+	switch ((uint32_t)sub_system) {
+	case MCUX_LPSPI0_CLK:
+		CLOCK_EnableClock(kCLOCK_Lpspi0);
+		break;
+	case MCUX_LPSPI1_CLK:
+		CLOCK_EnableClock(kCLOCK_Lpspi1);
+		break;
+	case MCUX_LPSPI2_CLK:
+		CLOCK_EnableClock(kCLOCK_Lpspi2);
+		break;
+	case MCUX_LPSPI3_CLK:
+		CLOCK_EnableClock(kCLOCK_Lpspi3);
+		break;
+	case MCUX_LPSPI4_CLK:
+		CLOCK_EnableClock(kCLOCK_Lpspi4);
+		break;
+	case MCUX_LPSPI5_CLK:
+		CLOCK_EnableClock(kCLOCK_Lpspi5);
+		break;
+	default:
+		break;
+	}
+#endif /* defined(CONFIG_SPI_NXP_LPSPI) */
+
+#if defined(CONFIG_I2C_MCUX_LPI2C)
+	switch ((uint32_t)sub_system) {
+	case MCUX_LPI2C0_CLK:
+		CLOCK_EnableClock(kCLOCK_Lpi2c0);
+		break;
+	case MCUX_LPI2C1_CLK:
+		CLOCK_EnableClock(kCLOCK_Lpi2c1);
+		break;
+	default:
+		break;
+	}
+#endif /* defined(CONFIG_I2C_MCUX_LPI2C) */
+
+	return 0;
+}
+
+static int mc_cgm_clock_control_off(const struct device *dev, clock_control_subsys_t sub_system)
+{
+	return 0;
+}
+
+static int mc_cgm_get_subsys_rate(const struct device *dev, clock_control_subsys_t sub_system,
+				  uint32_t *rate)
+{
+	uint32_t clock_name = (uint32_t)sub_system;
+
+	switch (clock_name) {
+#if defined(CONFIG_UART_MCUX_LPUART)
+	case MCUX_LPUART0_CLK:
+	case MCUX_LPUART8_CLK:
+		*rate = CLOCK_GetAipsPlatClkFreq();
+		break;
+	case MCUX_LPUART1_CLK:
+	case MCUX_LPUART2_CLK:
+	case MCUX_LPUART3_CLK:
+	case MCUX_LPUART4_CLK:
+	case MCUX_LPUART5_CLK:
+	case MCUX_LPUART6_CLK:
+	case MCUX_LPUART7_CLK:
+	case MCUX_LPUART9_CLK:
+	case MCUX_LPUART10_CLK:
+	case MCUX_LPUART11_CLK:
+	case MCUX_LPUART12_CLK:
+	case MCUX_LPUART13_CLK:
+	case MCUX_LPUART14_CLK:
+	case MCUX_LPUART15_CLK:
+		*rate = CLOCK_GetAipsSlowClkFreq();
+		break;
+#endif /* defined(CONFIG_UART_MCUX_LPUART) */
+
+#if defined(CONFIG_SPI_NXP_LPSPI)
+	case MCUX_LPSPI0_CLK:
+		*rate = CLOCK_GetAipsPlatClkFreq();
+		break;
+	case MCUX_LPSPI1_CLK:
+	case MCUX_LPSPI2_CLK:
+	case MCUX_LPSPI3_CLK:
+	case MCUX_LPSPI4_CLK:
+	case MCUX_LPSPI5_CLK:
+		*rate = CLOCK_GetAipsSlowClkFreq();
+		break;
+#endif /* defined(CONFIG_SPI_NXP_LPSPI) */
+
+#if defined(CONFIG_I2C_MCUX_LPI2C)
+	case MCUX_LPI2C0_CLK:
+	case MCUX_LPI2C1_CLK:
+		*rate = CLOCK_GetAipsSlowClkFreq();
+		break;
+#endif /* defined(CONFIG_I2C_MCUX_LPI2C) */
+
+#if defined(CONFIG_CAN_MCUX_FLEXCAN)
+	case MCUX_FLEXCAN0_CLK:
+		*rate = CLOCK_GetFlexcanPeClkFreq(0);
+		break;
+	case MCUX_FLEXCAN1_CLK:
+		*rate = CLOCK_GetFlexcanPeClkFreq(1);
+		break;
+	case MCUX_FLEXCAN2_CLK:
+		*rate = CLOCK_GetFlexcanPeClkFreq(2);
+		break;
+	case MCUX_FLEXCAN3_CLK:
+		*rate = CLOCK_GetFlexcanPeClkFreq(3);
+		break;
+	case MCUX_FLEXCAN4_CLK:
+		*rate = CLOCK_GetFlexcanPeClkFreq(4);
+		break;
+	case MCUX_FLEXCAN5_CLK:
+		*rate = CLOCK_GetFlexcanPeClkFreq(5);
+		break;
+#endif /* defined(CONFIG_CAN_MCUX_FLEXCAN) */
+	}
+	return 0;
+}
+
+static int mc_cgm_init(const struct device *dev)
+{
+#if defined(FSL_FEATURE_PMC_HAS_LAST_MILE_REGULATOR) && (FSL_FEATURE_PMC_HAS_LAST_MILE_REGULATOR)
+	/* Enables PMC last mile regulator before enable PLL.  */
+	if ((PMC->LVSC & PMC_LVSC_LVD15S_MASK) != 0U) {
+		/* External bipolar junction transistor is connected between external voltage and
+		 * V15 input pin.
+		 */
+		PMC->CONFIG |= PMC_CONFIG_LMBCTLEN_MASK;
+	}
+	while ((PMC->LVSC & PMC_LVSC_LVD15S_MASK) != 0U) {
+	}
+	PMC->CONFIG |= PMC_CONFIG_LMEN_MASK;
+	while ((PMC->CONFIG & PMC_CONFIG_LMSTAT_MASK) == 0u) {
+	}
+#endif /* FSL_FEATURE_PMC_HAS_LAST_MILE_REGULATOR */
+
+#if DT_NODE_HAS_COMPAT_STATUS(DT_NODELABEL(firc), nxp_firc, okay)
+	/* Switch the FIRC_DIV_SEL to the desired diveder. */
+	CLOCK_SetFircDiv(NXP_FIRC_DIV);
+	/* Disable FIRC in standby mode. */
+	CLOCK_DisableFircInStandbyMode();
+#endif
+
+#if DT_NODE_HAS_COMPAT_STATUS(DT_NODELABEL(sirc), nxp_sirc, okay)
+	/* Disable SIRC in standby mode. */
+	CLOCK_DisableSircInStandbyMode();
+#endif
+
+#if DT_NODE_HAS_COMPAT_STATUS(DT_NODELABEL(fxosc), nxp_fxosc, okay)
+	/* Enable FXOSC. */
+	CLOCK_InitFxosc(&fxosc_config);
+#endif
+
+#if DT_NODE_HAS_COMPAT_STATUS(DT_NODELABEL(pll), nxp_plldig, okay)
+	/* Enable PLL. */
+	CLOCK_InitPll(&pll_config);
+#endif
+
+#if DT_NODE_HAS_COMPAT_STATUS(DT_NODELABEL(mc_cgm), nxp_mc_cgm, okay)
+	CLOCK_SelectSafeClock(kFIRC_CLK_to_MUX0);
+	/* Configure MUX_0_CSC dividers */
+	CLOCK_SetClkMux0DivTriggerType(KCLOCK_CommonTriggerUpdate);
+	CLOCK_SetClkDiv(kCLOCK_DivCoreClk, NXP_PLL_MUX_0_DC_0_DIV);
+	CLOCK_SetClkDiv(kCLOCK_DivAipsPlatClk, NXP_PLL_MUX_0_DC_1_DIV);
+	CLOCK_SetClkDiv(kCLOCK_DivAipsSlowClk, NXP_PLL_MUX_0_DC_2_DIV);
+	CLOCK_SetClkDiv(kCLOCK_DivHseClk, NXP_PLL_MUX_0_DC_3_DIV);
+	CLOCK_SetClkDiv(kCLOCK_DivDcmClk, NXP_PLL_MUX_0_DC_4_DIV);
+#ifdef MC_CGM_MUX_0_DC_5_DIV_MASK
+	CLOCK_SetClkDiv(kCLOCK_DivLbistClk, NXP_PLL_MUX_0_DC_5_DIV);
+#endif
+#ifdef MC_CGM_MUX_0_DC_6_DIV_MASK
+	CLOCK_SetClkDiv(kCLOCK_DivQspiClk, NXP_PLL_MUX_0_DC_6_DIV);
+#endif
+	CLOCK_CommonTriggerClkMux0DivUpdate();
+	CLOCK_ProgressiveClockFrequencySwitch(kPLL_PHI0_CLK_to_MUX0, &pcfs_config);
+#endif
+
+	/* Set SystemCoreClock variable. */
+	SystemCoreClockUpdate();
+
+	return 0;
+}
+
+static DEVICE_API(clock_control, mcux_mcxe31x_clock_api) = {
+	.on = mc_cgm_clock_control_on,
+	.off = mc_cgm_clock_control_off,
+	.get_rate = mc_cgm_get_subsys_rate,
+};
+
+DEVICE_DT_INST_DEFINE(0, mc_cgm_init, NULL, NULL, NULL, PRE_KERNEL_1,
+		      CONFIG_CLOCK_CONTROL_INIT_PRIORITY, &mcux_mcxe31x_clock_api);

include/zephyr/dt-bindings/clock/nxp_mc_cgm.h
@@ -1,0 +1,113 @@
+/*
+ * Copyright 2025 NXP
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef ZEPHYR_INCLUDE_DT_BINDINGS_CLOCK_NXP_MC_CGM_H_
+#define ZEPHYR_INCLUDE_DT_BINDINGS_CLOCK_NXP_MC_CGM_H_
+
+/* Define a set of macros related to NXP mc_cgm IP configuration parameter */
+#define NXP_PLL_MAXIDOCHANGE   DT_PROP(DT_NODELABEL(mc_cgm), max_ido_change)
+#define NXP_PLL_STEPDURATION   DT_PROP(DT_NODELABEL(mc_cgm), step_duration)
+#define NXP_PLL_CLKSRCFREQ     DT_PROP(DT_NODELABEL(mc_cgm), clk_src_freq)
+#define NXP_PLL_MUX_0_DC_0_DIV DT_PROP(DT_NODELABEL(mc_cgm), mux_0_dc_0_div)
+#define NXP_PLL_MUX_0_DC_1_DIV DT_PROP(DT_NODELABEL(mc_cgm), mux_0_dc_1_div)
+#define NXP_PLL_MUX_0_DC_2_DIV DT_PROP(DT_NODELABEL(mc_cgm), mux_0_dc_2_div)
+#define NXP_PLL_MUX_0_DC_3_DIV DT_PROP(DT_NODELABEL(mc_cgm), mux_0_dc_3_div)
+#define NXP_PLL_MUX_0_DC_4_DIV DT_PROP(DT_NODELABEL(mc_cgm), mux_0_dc_4_div)
+#define NXP_PLL_MUX_0_DC_5_DIV DT_PROP(DT_NODELABEL(mc_cgm), mux_0_dc_5_div)
+#define NXP_PLL_MUX_0_DC_6_DIV DT_PROP(DT_NODELABEL(mc_cgm), mux_0_dc_6_div)
+
+/* Note- clock identifiers in this file must be unique,
+ * as the driver uses them in a switch case
+ */
+
+#define MCUX_MC_CGM_CLK_ID(high, low) ((high << 8) | (low))
+
+/* These IDs are used within SOC macros, and thus cannot be defined
+ * using the standard MCUX_MC_CGM_CLK_ID form
+ */
+/* --------------------- System layer clock --------------------- */
+#define MCUX_CORESYS_CLK  MCUX_MC_CGM_CLK_ID(0x00, 0x00)
+#define MCUX_AIPSPLAT_CLK MCUX_MC_CGM_CLK_ID(0x01, 0x00)
+#define MCUX_AIPSSLOW_CLK MCUX_MC_CGM_CLK_ID(0x02, 0x00)
+#define MCUX_HSE_CLK      MCUX_MC_CGM_CLK_ID(0x03, 0x00)
+#define MCUX_DCM_CLK      MCUX_MC_CGM_CLK_ID(0x04, 0x00)
+#define MCUX_LBIST_CLK    MCUX_MC_CGM_CLK_ID(0x05, 0x00)
+#define MCUX_QSPI_CLK     MCUX_MC_CGM_CLK_ID(0x06, 0x00)
+
+/* --------------------- MC_CGM clock --------------------- */
+#define MCUX_FIRC_CLK    MCUX_MC_CGM_CLK_ID(0x10, 0x00)
+#define MCUX_SIRC_CLK    MCUX_MC_CGM_CLK_ID(0x11, 0x00)
+#define MCUX_FXOSC_CLK   MCUX_MC_CGM_CLK_ID(0x12, 0x00)
+#define MCUX_SXOSC_CLK   MCUX_MC_CGM_CLK_ID(0x13, 0x00)
+#define MCUX_PLLPHI0_CLK MCUX_MC_CGM_CLK_ID(0x14, 0x00)
+#define MCUX_PLLPHI1_CLK MCUX_MC_CGM_CLK_ID(0x14, 0x01)
+
+/* --------------------- Peripheral clock --------------------- */
+#define MCUX_ADC0_CLK MCUX_MC_CGM_CLK_ID(0x20, 0x00)
+#define MCUX_ADC1_CLK MCUX_MC_CGM_CLK_ID(0x20, 0x01)
+#define MCUX_ADC2_CLK MCUX_MC_CGM_CLK_ID(0x20, 0x02)
+
+#define MCUX_BCTU_CLK MCUX_MC_CGM_CLK_ID(0x21, 0x00)
+
+#define MCUX_CMP0_CLK MCUX_MC_CGM_CLK_ID(0x22, 0x00)
+#define MCUX_CMP1_CLK MCUX_MC_CGM_CLK_ID(0x22, 0x01)
+#define MCUX_CMP2_CLK MCUX_MC_CGM_CLK_ID(0x22, 0x02)
+
+#define MCUX_EMIOS_CLK MCUX_MC_CGM_CLK_ID(0x23, 0x00)
+
+#define MCUX_FLEXCAN0_CLK MCUX_MC_CGM_CLK_ID(0x24, 0x00)
+#define MCUX_FLEXCAN1_CLK MCUX_MC_CGM_CLK_ID(0x24, 0x01)
+#define MCUX_FLEXCAN2_CLK MCUX_MC_CGM_CLK_ID(0x24, 0x02)
+#define MCUX_FLEXCAN3_CLK MCUX_MC_CGM_CLK_ID(0x24, 0x03)
+#define MCUX_FLEXCAN4_CLK MCUX_MC_CGM_CLK_ID(0x24, 0x04)
+#define MCUX_FLEXCAN5_CLK MCUX_MC_CGM_CLK_ID(0x24, 0x05)
+
+#define MCUX_FLEXIO_CLK MCUX_MC_CGM_CLK_ID(0x25, 0x00)
+
+#define MCUX_LPI2C0_CLK MCUX_MC_CGM_CLK_ID(0x26, 0x00)
+#define MCUX_LPI2C1_CLK MCUX_MC_CGM_CLK_ID(0x26, 0x01)
+
+#define MCUX_LPSPI0_CLK MCUX_MC_CGM_CLK_ID(0x27, 0x00)
+#define MCUX_LPSPI1_CLK MCUX_MC_CGM_CLK_ID(0x27, 0x01)
+#define MCUX_LPSPI2_CLK MCUX_MC_CGM_CLK_ID(0x27, 0x02)
+#define MCUX_LPSPI3_CLK MCUX_MC_CGM_CLK_ID(0x27, 0x03)
+#define MCUX_LPSPI4_CLK MCUX_MC_CGM_CLK_ID(0x27, 0x04)
+#define MCUX_LPSPI5_CLK MCUX_MC_CGM_CLK_ID(0x27, 0x05)
+
+#define MCUX_LPUART0_CLK  MCUX_MC_CGM_CLK_ID(0x28, 0x00)
+#define MCUX_LPUART1_CLK  MCUX_MC_CGM_CLK_ID(0x28, 0x01)
+#define MCUX_LPUART2_CLK  MCUX_MC_CGM_CLK_ID(0x28, 0x02)
+#define MCUX_LPUART3_CLK  MCUX_MC_CGM_CLK_ID(0x28, 0x03)
+#define MCUX_LPUART4_CLK  MCUX_MC_CGM_CLK_ID(0x28, 0x04)
+#define MCUX_LPUART5_CLK  MCUX_MC_CGM_CLK_ID(0x28, 0x05)
+#define MCUX_LPUART6_CLK  MCUX_MC_CGM_CLK_ID(0x28, 0x06)
+#define MCUX_LPUART7_CLK  MCUX_MC_CGM_CLK_ID(0x28, 0x07)
+#define MCUX_LPUART8_CLK  MCUX_MC_CGM_CLK_ID(0x28, 0x08)
+#define MCUX_LPUART9_CLK  MCUX_MC_CGM_CLK_ID(0x28, 0x09)
+#define MCUX_LPUART10_CLK MCUX_MC_CGM_CLK_ID(0x28, 0x0A)
+#define MCUX_LPUART11_CLK MCUX_MC_CGM_CLK_ID(0x28, 0x0B)
+#define MCUX_LPUART12_CLK MCUX_MC_CGM_CLK_ID(0x28, 0x0C)
+#define MCUX_LPUART13_CLK MCUX_MC_CGM_CLK_ID(0x28, 0x0D)
+#define MCUX_LPUART14_CLK MCUX_MC_CGM_CLK_ID(0x28, 0x0E)
+#define MCUX_LPUART15_CLK MCUX_MC_CGM_CLK_ID(0x28, 0x0F)
+
+#define MCUX_PIT0_CLK MCUX_MC_CGM_CLK_ID(0x29, 0x00)
+#define MCUX_PIT1_CLK MCUX_MC_CGM_CLK_ID(0x29, 0x01)
+#define MCUX_PIT2_CLK MCUX_MC_CGM_CLK_ID(0x29, 0x02)
+
+#define MCUX_SAI0_CLK MCUX_MC_CGM_CLK_ID(0x2A, 0x00)
+#define MCUX_SAI1_CLK MCUX_MC_CGM_CLK_ID(0x2A, 0x01)
+
+#define MCUX_STM0_CLK MCUX_MC_CGM_CLK_ID(0x2B, 0x00)
+#define MCUX_STM1_CLK MCUX_MC_CGM_CLK_ID(0x2B, 0x01)
+
+/* --------------------- Partition 2 clock --------------------- */
+#define MCUX_QSPISF_CLK MCUX_MC_CGM_CLK_ID(0x2C, 0x00)
+#define MCUX_EMACRX_CLK MCUX_MC_CGM_CLK_ID(0x2C, 0x01)
+#define MCUX_EMACTX_CLK MCUX_MC_CGM_CLK_ID(0x2C, 0x02)
+#define MCUX_EMACTS_CLK MCUX_MC_CGM_CLK_ID(0x2C, 0x03)
+
+#endif /* ZEPHYR_INCLUDE_DT_BINDINGS_CLOCK_NXP_MC_CGM_H_ */