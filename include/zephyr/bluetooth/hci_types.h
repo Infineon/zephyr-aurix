zephyrproject-rtos/zephyr

Include: Bluetooth: Add HCI voice_setting definitions
Add macro `BT_HCI_VOICE_SETTINGS` to assemble the voice_setting

Add macro `BT_HCI_VOICE_SETTING_*_PREP` to set the fields for
voice_setting.

Add macro `BT_HCI_VOICE_SETTING_*_GET` to get the value of the
field from the voice_setting.

Signed-off-by: Lyle Zhu <lyle.zhu@nxp.com>

include/zephyr/bluetooth/hci_types.h
@@ -426,6 +426,58 @@ struct bt_hci_cp_accept_conn_req {
 	uint8_t   role;
 } __packed;
 
+#define BT_HCI_VOICE_SETTING_AIR_CODING_FMT_CVSD    0x00
+#define BT_HCI_VOICE_SETTING_AIR_CODING_ULAW        0x01
+#define BT_HCI_VOICE_SETTING_AIR_CODING_ALAW        0x02
+#define BT_HCI_VOICE_SETTING_AIR_CODING_TRANSPARENT 0x03
+
+#define BT_HCI_VOICE_SETTING_PCM_BIT_POS_DEFAULT 0x00
+
+#define BT_HCI_VOICE_SETTING_SAMPLE_SIZE_8_BITS  0x00
+#define BT_HCI_VOICE_SETTING_SAMPLE_SIZE_16_BITS 0x01
+
+#define BT_HCI_VOICE_SETTING_DATA_FMT_1_COMPLEMENT 0x00
+#define BT_HCI_VOICE_SETTING_DATA_FMT_2_COMPLEMENT 0x01
+#define BT_HCI_VOICE_SETTING_DATA_FMT_SIGNED       0x02
+#define BT_HCI_VOICE_SETTING_DATA_FMT_UNSIGNED     0x03
+
+#define BT_HCI_VOICE_SETTING_CODING_FMT_LINEAR 0x00
+#define BT_HCI_VOICE_SETTING_CODING_FMT_ULAW   0x01
+#define BT_HCI_VOICE_SETTING_CODING_FMT_ALAW   0x02
+
+#define BT_HCI_VOICE_SETTING_AIR_CODING_FMT_MASK GENMASK(1, 0)
+#define BT_HCI_VOICE_SETTING_PCM_BIT_POS_MASK    GENMASK(2, 4)
+#define BT_HCI_VOICE_SETTING_SAMPLE_SIZE_MASK    GENMASK(5, 5)
+#define BT_HCI_VOICE_SETTING_DATA_FMT_MASK       GENMASK(7, 6)
+#define BT_HCI_VOICE_SETTING_CODING_FMT_MASK     GENMASK(9, 8)
+
+#define BT_HCI_VOICE_SETTING_AIR_CODING_FMT_PREP(val) \
+	FIELD_PREP(BT_HCI_VOICE_SETTING_AIR_CODING_FMT_MASK, val)
+#define BT_HCI_VOICE_SETTING_PCM_BIT_POS_PREP(val) \
+	FIELD_PREP(BT_HCI_VOICE_SETTING_PCM_BIT_POS_MASK, val)
+#define BT_HCI_VOICE_SETTING_SAMPLE_SIZE_PREP(val) \
+	FIELD_PREP(BT_HCI_VOICE_SETTING_SAMPLE_SIZE_MASK, val)
+#define BT_HCI_VOICE_SETTING_DATA_FMT_PREP(val) FIELD_PREP(BT_HCI_VOICE_SETTING_DATA_FMT_MASK, val)
+#define BT_HCI_VOICE_SETTING_CODING_FMT_PREP(val) \
+	FIELD_PREP(BT_HCI_VOICE_SETTING_CODING_FMT_MASK, val)
+
+#define BT_HCI_VOICE_SETTING_AIR_CODING_FMT_GET(val) \
+	FIELD_GET(BT_HCI_VOICE_SETTING_AIR_CODING_FMT_MASK, val)
+#define BT_HCI_VOICE_SETTING_PCM_BIT_POS_GET(val) \
+	FIELD_GET(BT_HCI_VOICE_SETTING_PCM_BIT_POS_MASK, val)
+#define BT_HCI_VOICE_SETTING_SAMPLE_SIZE_GET(val) \
+	FIELD_GET(BT_HCI_VOICE_SETTING_SAMPLE_SIZE_MASK, val)
+#define BT_HCI_VOICE_SETTING_DATA_FMT_GET(val) FIELD_GET(BT_HCI_VOICE_SETTING_DATA_FMT_MASK, val)
+#define BT_HCI_VOICE_SETTING_CODING_FMT_GET(val) \
+	FIELD_GET(BT_HCI_VOICE_SETTING_CODING_FMT_MASK, val)
+
+#define BT_HCI_VOICE_SETTINGS(air_coding_fmt, pcm_bit_pos, sample_size, data_fmt, coding_fmt) \
+	(BT_HCI_VOICE_SETTING_AIR_CODING_FMT_PREP(air_coding_fmt) | \
+	 BT_HCI_VOICE_SETTING_PCM_BIT_POS_PREP(pcm_bit_pos) | \
+	 BT_HCI_VOICE_SETTING_SAMPLE_SIZE_PREP(sample_size) | \
+	 BT_HCI_VOICE_SETTING_DATA_FMT_PREP(data_fmt) | \
+	 BT_HCI_VOICE_SETTING_CODING_FMT_PREP(coding_fmt))
+
 #define BT_HCI_OP_SETUP_SYNC_CONN               BT_OP(BT_OGF_LINK_CTRL, 0x0028) /* 0x0428 */
 struct bt_hci_cp_setup_sync_conn {
 	uint16_t  handle;

Infineon/zephyr-aurix

include/zephyr/bluetooth/hci_types.h
@@ -427,6 +427,58 @@ struct bt_hci_cp_accept_conn_req {
 	uint8_t   role;
 } __packed;
 
+#define BT_HCI_VOICE_SETTING_AIR_CODING_FMT_CVSD        0x00
+#define BT_HCI_VOICE_SETTING_AIR_CODING_FMT_ULAW        0x01
+#define BT_HCI_VOICE_SETTING_AIR_CODING_FMT_ALAW        0x02
+#define BT_HCI_VOICE_SETTING_AIR_CODING_FMT_TRANSPARENT 0x03
+
+#define BT_HCI_VOICE_SETTING_PCM_BIT_POS_DEFAULT 0x00
+
+#define BT_HCI_VOICE_SETTING_SAMPLE_SIZE_8_BITS  0x00
+#define BT_HCI_VOICE_SETTING_SAMPLE_SIZE_16_BITS 0x01
+
+#define BT_HCI_VOICE_SETTING_DATA_FMT_1_COMPLEMENT 0x00
+#define BT_HCI_VOICE_SETTING_DATA_FMT_2_COMPLEMENT 0x01
+#define BT_HCI_VOICE_SETTING_DATA_FMT_SIGNED       0x02
+#define BT_HCI_VOICE_SETTING_DATA_FMT_UNSIGNED     0x03
+
+#define BT_HCI_VOICE_SETTING_CODING_FMT_LINEAR 0x00
+#define BT_HCI_VOICE_SETTING_CODING_FMT_ULAW   0x01
+#define BT_HCI_VOICE_SETTING_CODING_FMT_ALAW   0x02
+
+#define BT_HCI_VOICE_SETTING_AIR_CODING_FMT_MASK GENMASK(1, 0)
+#define BT_HCI_VOICE_SETTING_PCM_BIT_POS_MASK    GENMASK(2, 4)
+#define BT_HCI_VOICE_SETTING_SAMPLE_SIZE_MASK    GENMASK(5, 5)
+#define BT_HCI_VOICE_SETTING_DATA_FMT_MASK       GENMASK(7, 6)
+#define BT_HCI_VOICE_SETTING_CODING_FMT_MASK     GENMASK(9, 8)
+
+#define BT_HCI_VOICE_SETTING_AIR_CODING_FMT_PREP(val) \
+	FIELD_PREP(BT_HCI_VOICE_SETTING_AIR_CODING_FMT_MASK, val)
+#define BT_HCI_VOICE_SETTING_PCM_BIT_POS_PREP(val) \
+	FIELD_PREP(BT_HCI_VOICE_SETTING_PCM_BIT_POS_MASK, val)
+#define BT_HCI_VOICE_SETTING_SAMPLE_SIZE_PREP(val) \
+	FIELD_PREP(BT_HCI_VOICE_SETTING_SAMPLE_SIZE_MASK, val)
+#define BT_HCI_VOICE_SETTING_DATA_FMT_PREP(val) FIELD_PREP(BT_HCI_VOICE_SETTING_DATA_FMT_MASK, val)
+#define BT_HCI_VOICE_SETTING_CODING_FMT_PREP(val) \
+	FIELD_PREP(BT_HCI_VOICE_SETTING_CODING_FMT_MASK, val)
+
+#define BT_HCI_VOICE_SETTING_AIR_CODING_FMT_GET(val) \
+	FIELD_GET(BT_HCI_VOICE_SETTING_AIR_CODING_FMT_MASK, val)
+#define BT_HCI_VOICE_SETTING_PCM_BIT_POS_GET(val) \
+	FIELD_GET(BT_HCI_VOICE_SETTING_PCM_BIT_POS_MASK, val)
+#define BT_HCI_VOICE_SETTING_SAMPLE_SIZE_GET(val) \
+	FIELD_GET(BT_HCI_VOICE_SETTING_SAMPLE_SIZE_MASK, val)
+#define BT_HCI_VOICE_SETTING_DATA_FMT_GET(val) FIELD_GET(BT_HCI_VOICE_SETTING_DATA_FMT_MASK, val)
+#define BT_HCI_VOICE_SETTING_CODING_FMT_GET(val) \
+	FIELD_GET(BT_HCI_VOICE_SETTING_CODING_FMT_MASK, val)
+
+#define BT_HCI_VOICE_SETTINGS(air_coding_fmt, pcm_bit_pos, sample_size, data_fmt, coding_fmt) \
+	(BT_HCI_VOICE_SETTING_AIR_CODING_FMT_PREP(air_coding_fmt) | \
+	 BT_HCI_VOICE_SETTING_PCM_BIT_POS_PREP(pcm_bit_pos) | \
+	 BT_HCI_VOICE_SETTING_SAMPLE_SIZE_PREP(sample_size) | \
+	 BT_HCI_VOICE_SETTING_DATA_FMT_PREP(data_fmt) | \
+	 BT_HCI_VOICE_SETTING_CODING_FMT_PREP(coding_fmt))
+
 #define BT_HCI_OP_SETUP_SYNC_CONN               BT_OP(BT_OGF_LINK_CTRL, 0x0028) /* 0x0428 */
 struct bt_hci_cp_setup_sync_conn {
 	uint16_t  handle;