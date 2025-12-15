zephyrproject-rtos/zephyr

samples: lora: send: display packet airtime
Display the expected airtime of the transmitted packet at the start of
the sample.

Signed-off-by: Jordan Yates <jordan@embeint.com>

samples/drivers/lora/send/src/main.c
@@ -49,6 +49,8 @@ int main(void)
 		return 0;
 	}
 
+	LOG_INF("Expected packet airtime: %d ms", lora_airtime(lora_dev, MAX_DATA_LEN));
+
 	while (1) {
 		ret = lora_send(lora_dev, data, MAX_DATA_LEN);
 		if (ret < 0) {

Infineon/zephyr-aurix

samples/drivers/lora/send/src/main.c
@@ -49,6 +49,8 @@ int main(void)
 		return 0;
 	}
 
+	LOG_INF("Expected packet airtime: %u ms", lora_airtime(lora_dev, MAX_DATA_LEN));
+
 	while (1) {
 		ret = lora_send(lora_dev, data, MAX_DATA_LEN);
 		if (ret < 0) {