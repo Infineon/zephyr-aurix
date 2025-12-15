zephyrproject-rtos/zephyr

tests: counter_basic_api: harden alarm capability probing
- Add a self-contained alarm_capable() probe that starts the device, sets
  and cancels a 1-tick alarm, and stops the counter again. This treats
  -ENOTSUP as “not capable” and avoids leaving pending alarms.
- Switch single/multiple alarm capability checks to reuse alarm_capable(),
  preventing unsupported drivers from running alarm cases and reducing
  false failures.
- Ensure capability probing no longer leaves the driver running or with
  active alarms, preventing teardown/set_top_value failures in later tests.

Signed-off-by: Zhaoxiang Jin <Zhaoxiang.Jin_1@nxp.com>

tests/drivers/counter/counter_basic_api/src/test_counter.c
@@ -432,6 +432,54 @@ static void alarm_handler(const struct device *dev, uint8_t chan_id,
 	k_sem_give(&alarm_cnt_sem);
 }
 
+static bool alarm_capable(const struct device *dev)
+{
+	struct counter_alarm_cfg cfg = {
+		.flags = 0,
+		.ticks = 1U,
+		.callback = alarm_handler,
+		.user_data = &cntr_alarm_cfg,
+	};
+	int err;
+	bool started = false;
+
+	if (counter_get_num_of_channels(dev) < 1U) {
+		return false;
+	}
+
+	err = counter_start(dev);
+	if (err == 0) {
+		started = true;
+	} else if (err == -ENOTSUP) {
+		return false;
+	} else {
+		return false;
+	}
+
+	err = counter_set_channel_alarm(dev, 0, &cfg);
+	if (err == -ENOTSUP) {
+		goto out_stop;
+	}
+
+	if (err != 0) {
+		goto out_stop;
+	}
+
+	err = counter_cancel_channel_alarm(dev, 0);
+	if (err == -ENOTSUP) {
+		goto out_stop;
+	}
+
+out_stop:
+	if (started) {
+		int stop_err = counter_stop(dev);
+
+		ARG_UNUSED(stop_err);
+	}
+
+	return err == 0;
+}
+
 static void test_single_shot_alarm_instance(const struct device *dev, bool set_top)
 {
 	int err;
@@ -524,13 +572,12 @@ void test_single_shot_alarm_top_instance(const struct device *dev)
 
 static bool single_channel_alarm_capable(const struct device *dev)
 {
-	return (counter_get_num_of_channels(dev) > 0);
+	return alarm_capable(dev);
 }
 
 static bool single_channel_alarm_and_custom_top_capable(const struct device *dev)
 {
-	return single_channel_alarm_capable(dev) &&
-		set_top_value_capable(dev);
+	return alarm_capable(dev) && set_top_value_capable(dev);
 }
 
 ZTEST(counter_basic, test_single_shot_alarm_notop)
@@ -652,7 +699,7 @@ static void test_multiple_alarms_instance(const struct device *dev)
 
 static bool multiple_channel_alarm_capable(const struct device *dev)
 {
-	return (counter_get_num_of_channels(dev) > 1);
+	return alarm_capable(dev) && (counter_get_num_of_channels(dev) > 1);
 }
 
 ZTEST(counter_basic, test_multiple_alarms)

Infineon/zephyr-aurix

tests/drivers/counter/counter_basic_api/src/test_counter.c
@@ -432,6 +432,62 @@ static void alarm_handler(const struct device *dev, uint8_t chan_id,
 	k_sem_give(&alarm_cnt_sem);
 }
 
+static void alarm_capable_handler(const struct device *dev, uint8_t chan_id,
+				    uint32_t counter,
+				    void *user_data)
+{
+	ARG_UNUSED(dev);
+	ARG_UNUSED(chan_id);
+	ARG_UNUSED(counter);
+	ARG_UNUSED(user_data);
+
+	/* Intentionally empty - capability probe only */
+}
+
+/* Any non-zero error (including -ENOTSUP) is treated as "not supported",
+ * and only returns true when there are no errors throughout the entire process.
+ */
+static bool alarm_capable(const struct device *dev)
+{
+	struct counter_alarm_cfg cfg = {
+		.flags = 0U,
+		.ticks = counter_us_to_ticks(dev, 1000U),
+		.callback = alarm_capable_handler,
+		.user_data = NULL,
+	};
+
+	/* Avoid zero-tick requests on very low-frequency counters */
+	if (cfg.ticks == 0U) {
+		cfg.ticks = 1U;
+	}
+
+	int err;
+
+	if (counter_get_num_of_channels(dev) < 1U) {
+		return false;
+	}
+
+	err = counter_start(dev);
+	if (err != 0) {
+		return false;
+	}
+
+	err = counter_set_channel_alarm(dev, 0, &cfg);
+	if (err != 0) {
+		goto out_stop;
+	}
+
+	err = counter_cancel_channel_alarm(dev, 0);
+	if (err != 0) {
+		goto out_stop;
+	}
+
+out_stop:
+	(void)counter_stop(dev);
+
+	return err == 0;
+}
+
 static void test_single_shot_alarm_instance(const struct device *dev, bool set_top)
 {
 	int err;
@@ -524,13 +580,12 @@ void test_single_shot_alarm_top_instance(const struct device *dev)
 
 static bool single_channel_alarm_capable(const struct device *dev)
 {
-	return (counter_get_num_of_channels(dev) > 0);
+	return alarm_capable(dev);
 }
 
 static bool single_channel_alarm_and_custom_top_capable(const struct device *dev)
 {
-	return single_channel_alarm_capable(dev) &&
-		set_top_value_capable(dev);
+	return alarm_capable(dev) && set_top_value_capable(dev);
 }
 
 ZTEST(counter_basic, test_single_shot_alarm_notop)
@@ -652,7 +707,7 @@ static void test_multiple_alarms_instance(const struct device *dev)
 
 static bool multiple_channel_alarm_capable(const struct device *dev)
 {
-	return (counter_get_num_of_channels(dev) > 1);
+	return alarm_capable(dev) && (counter_get_num_of_channels(dev) > 1);
 }
 
 ZTEST(counter_basic, test_multiple_alarms)