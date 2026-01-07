Infineon/zephyr-aurix

commit a93471f9203408117ca7b0ae0e32f894935a56b0

drivers: can: mcan: remove useless asserts
Remove useless asserts. These indexes are determined via a for loop which
already uses the asserted value as its maximum.

Signed-off-by: Henrik Brix Andersen <henrik@brixandersen.dk>

drivers/can/can_mcan.c
@@ -1053,7 +1053,6 @@ int can_mcan_send(const struct device *dev, const struct can_frame *frame, k_tim
 		}
 	}
 
-	__ASSERT_NO_MSG(put_idx < cbs->num_tx);
 	cbs->tx[put_idx].function = callback;
 	cbs->tx[put_idx].user_data = user_data;
 
@@ -1134,7 +1133,6 @@ int can_mcan_add_rx_filter_std(const struct device *dev, can_rx_callback_t callb
 
 	LOG_DBG("Attached std filter at %d", filter_id);
 
-	__ASSERT_NO_MSG(filter_id < cbs->num_std);
 	cbs->std[filter_id].function = callback;
 	cbs->std[filter_id].user_data = user_data;
 
@@ -1185,7 +1183,6 @@ static int can_mcan_add_rx_filter_ext(const struct device *dev, can_rx_callback_
 
 	LOG_DBG("Attached ext filter at %d", filter_id);
 
-	__ASSERT_NO_MSG(filter_id < cbs->num_ext);
 	cbs->ext[filter_id].function = callback;
 	cbs->ext[filter_id].user_data = user_data;
 

commit 6cee3f45427cbcbbb43e56612771f26500d707d1

drivers: can: mcan: reintroduce assert in can_mcan_send()
Reintroduce the assert on put_idx being within the limit in can_mcan_send()
but move it to just after the value was determined and add a comment on its
purpose.

With the current code, this will never get triggered due to the tx_sem
being acquired before (meaning a free TX slot was acquired).

Avoid assigning a signed initial literal to an unassigned type.

Fixes: a93471f9203408117ca7b0ae0e32f894935a56b0

Signed-off-by: Henrik Brix Andersen <hebad@vestas.com>

drivers/can/can_mcan.c
@@ -950,7 +950,7 @@ int can_mcan_send(const struct device *dev, const struct can_frame *frame, k_tim
 #endif /* !CONFIG_CAN_FD_MODE */
 		.efc = 1U,
 	};
-	uint32_t put_idx = -1;
+	uint32_t put_idx = UINT32_MAX;
 	uint32_t reg;
 	int err;
 
@@ -1025,6 +1025,8 @@ int can_mcan_send(const struct device *dev, const struct can_frame *frame, k_tim
 		}
 	}
 
+	/* A free TX buffer should always be available since the data->tx_sem was acquired */
+	__ASSERT_NO_MSG(put_idx < cbs->num_tx);
 	tx_hdr.mm = put_idx;
 
 	if ((frame->flags & CAN_FRAME_IDE) != 0U) {