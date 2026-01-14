zephyrproject-rtos/zephyr

tests: Add support for nRF7120 to CI tests
Initial porting of nRF7120 to zephyr requires support for a number of
tests that are run as part of the CI. This commit adds the support.

Signed-off-by: Robert Robinson <robert.robinson@nordicsemi.no>

tests/arch/common/gen_isr_table/src/main.c
@@ -42,6 +42,11 @@ extern const uintptr_t _irq_vector_table[];
 #define ISR3_OFFSET	15
 #define ISR5_OFFSET	16
 #define TRIG_CHECK_SIZE	17
+#elif defined(CONFIG_SOC_SERIES_NRF71X) && defined(CONFIG_RISCV_CORE_NORDIC_VPR)
+#define ISR1_OFFSET	16
+#define ISR3_OFFSET	21
+#define ISR5_OFFSET	22
+#define TRIG_CHECK_SIZE	23
 #elif defined(CONFIG_SOC_NRF9280_CPUPPR)
 #define ISR1_OFFSET	14
 #define ISR3_OFFSET	15

tests/arch/common/interrupt/src/nested_irq.c
@@ -75,6 +75,12 @@
 
 #define IRQ0_PRIO	1
 #define IRQ1_PRIO	2
+#elif defined(CONFIG_SOC_SERIES_NRF71X) && defined(CONFIG_RISCV_CORE_NORDIC_VPR)
+#define IRQ0_LINE	19
+#define IRQ1_LINE	20
+
+#define IRQ0_PRIO	1
+#define IRQ1_PRIO	2
 #elif defined(CONFIG_SOC_NRF9280_CPUPPR)
 #define IRQ0_LINE	14
 #define IRQ1_LINE	15

Infineon/zephyr-aurix

tests/arch/common/gen_isr_table/src/main.c
@@ -42,6 +42,11 @@ extern const uintptr_t _irq_vector_table[];
 #define ISR3_OFFSET	15
 #define ISR5_OFFSET	16
 #define TRIG_CHECK_SIZE	17
+#elif defined(CONFIG_SOC_SERIES_NRF71) && defined(CONFIG_RISCV_CORE_NORDIC_VPR)
+#define ISR1_OFFSET	16
+#define ISR3_OFFSET	21
+#define ISR5_OFFSET	22
+#define TRIG_CHECK_SIZE	23
 #elif defined(CONFIG_SOC_NRF9280_CPUPPR)
 #define ISR1_OFFSET	14
 #define ISR3_OFFSET	15

tests/arch/common/interrupt/src/nested_irq.c
@@ -75,6 +75,12 @@
 
 #define IRQ0_PRIO	1
 #define IRQ1_PRIO	2
+#elif defined(CONFIG_SOC_SERIES_NRF71) && defined(CONFIG_RISCV_CORE_NORDIC_VPR)
+#define IRQ0_LINE	19
+#define IRQ1_LINE	20
+
+#define IRQ0_PRIO	1
+#define IRQ1_PRIO	2
 #elif defined(CONFIG_SOC_NRF9280_CPUPPR)
 #define IRQ0_LINE	14
 #define IRQ1_LINE	15