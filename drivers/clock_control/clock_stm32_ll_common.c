zephyrproject-rtos/zephyr

drivers: clock: stm32: update stm32f2_f4_f7 driver with the new binding
This commit updates the STM32Fx clock driver to add complete support for
all PLLs for all SoCs.

Supports  all outputs and additional divisors:
- PLL-DIV-R
- PLLI2S-P
- PLLI2S-DIV-Q
- PLLI2S-DIV-R

Adds global checks to make sure that all:
- All PLLs share the same source clocks
- All PLLs share the same M-Divisor (on applicable SoCs)
- Both div-X and div-divX are defined (on applicable SoCs)

Functions get_plli2s_source and get_plli2ssrc_frequency are added to make
sure that PLLI2S can be used even if PLL is not defined.

Signed-off-by: Guillaume Gautier <guillaume.gautier-ext@st.com>

drivers/clock_control/clock_stm32_ll_common.c
@@ -222,6 +222,20 @@ int enabled_clock(uint32_t src_clk)
 		}
 		break;
 #endif /* STM32_SRC_PLL_R */
+#if defined(STM32_SRC_PLL_DIVR)
+	case STM32_SRC_PLL_DIVR:
+		if (!IS_ENABLED(STM32_PLL_R_ENABLED)) {
+			r = -ENOTSUP;
+		}
+		break;
+#endif /* STM32_SRC_PLL_DIVR */
+#if defined(STM32_SRC_PLLI2S_P)
+	case STM32_SRC_PLLI2S_P:
+		if (!IS_ENABLED(STM32_PLLI2S_P_ENABLED)) {
+			r = -ENOTSUP;
+		}
+		break;
+#endif /* STM32_SRC_PLLI2S_P */
 #if defined(STM32_SRC_PLLI2S_Q)
 	case STM32_SRC_PLLI2S_Q:
 		if (!IS_ENABLED(STM32_PLLI2S_Q_ENABLED)) {
@@ -229,6 +243,13 @@ int enabled_clock(uint32_t src_clk)
 		}
 		break;
 #endif /* STM32_SRC_PLLI2S_Q */
+#if defined(STM32_SRC_PLLI2S_DIVQ)
+	case STM32_SRC_PLLI2S_DIVQ:
+		if (!IS_ENABLED(STM32_PLLI2S_Q_ENABLED)) {
+			r = -ENOTSUP;
+		}
+		break;
+#endif /* STM32_SRC_PLLI2S_DIVQ */
 #if defined(STM32_SRC_PLLI2S_R)
 	case STM32_SRC_PLLI2S_R:
 		if (!IS_ENABLED(STM32_PLLI2S_R_ENABLED)) {
@@ -236,6 +257,13 @@ int enabled_clock(uint32_t src_clk)
 		}
 		break;
 #endif /* STM32_SRC_PLLI2S_R */
+#if defined(STM32_SRC_PLLI2S_DIVR)
+	case STM32_SRC_PLLI2S_DIVR:
+		if (!IS_ENABLED(STM32_PLLI2S_R_ENABLED)) {
+			r = -ENOTSUP;
+		}
+		break;
+#endif /* STM32_SRC_PLLI2S_DIVR */
 #if defined(STM32_SRC_PLLSAI_P)
 	case STM32_SRC_PLLSAI_P:
 		if (!IS_ENABLED(STM32_PLLSAI_P_ENABLED)) {
@@ -524,22 +552,57 @@ static int stm32_clock_control_get_subsys_rate(const struct device *clock,
 					      STM32_PLL_R_DIVISOR);
 		break;
 #endif
-#if defined(STM32_SRC_PLLI2S_Q) && STM32_PLLI2S_Q_ENABLED && STM32_PLLI2S_ENABLED
-	case STM32_SRC_PLLI2S_Q:
+#if defined(STM32_SRC_PLL_DIVR) && STM32_PLL_R_ENABLED && STM32_PLL_DIVR_ENABLED
+	case STM32_SRC_PLL_DIVR:
 		*rate = get_pll_div_frequency(get_pllsrc_frequency(),
+					      STM32_PLL_M_DIVISOR,
+					      STM32_PLL_N_MULTIPLIER,
+					      STM32_PLL_R_DIVISOR);
+		*rate /= STM32_PLL_DIVR_DIVISOR;
+		break;
+#endif
+#if defined(STM32_SRC_PLLI2S_P) && STM32_PLLI2S_P_ENABLED
+	case STM32_SRC_PLLI2S_P:
+		*rate = get_pll_div_frequency(get_plli2ssrc_frequency(),
+					      STM32_PLLI2S_M_DIVISOR,
+					      STM32_PLLI2S_N_MULTIPLIER,
+					      STM32_PLLI2S_P_DIVISOR);
+		break;
+#endif /* STM32_SRC_PLLI2S_P */
+#if defined(STM32_SRC_PLLI2S_Q) && STM32_PLLI2S_Q_ENABLED
+	case STM32_SRC_PLLI2S_Q:
+		*rate = get_pll_div_frequency(get_plli2ssrc_frequency(),
 					      STM32_PLLI2S_M_DIVISOR,
 					      STM32_PLLI2S_N_MULTIPLIER,
 					      STM32_PLLI2S_Q_DIVISOR);
 		break;
 #endif /* STM32_SRC_PLLI2S_Q */
-#if defined(STM32_SRC_PLLI2S_R) && STM32_PLLI2S_ENABLED
+#if defined(STM32_SRC_PLLI2S_DIVQ) && STM32_PLLI2S_Q_ENABLED && STM32_PLLI2S_DIVQ_ENABLED
+	case STM32_SRC_PLLI2S_DIVQ:
+		*rate = get_pll_div_frequency(get_plli2ssrc_frequency(),
+					      STM32_PLLI2S_M_DIVISOR,
+					      STM32_PLLI2S_N_MULTIPLIER,
+					      STM32_PLLI2S_Q_DIVISOR);
+		*rate /= STM32_PLLI2S_DIVQ_DIVISOR;
+		break;
+#endif /* STM32_SRC_PLLI2S_DIVQ */
+#if defined(STM32_SRC_PLLI2S_R) && STM32_PLLI2S_R_ENABLED
 	case STM32_SRC_PLLI2S_R:
-		*rate = get_pll_div_frequency(get_pllsrc_frequency(),
+		*rate = get_pll_div_frequency(get_plli2ssrc_frequency(),
 					      STM32_PLLI2S_M_DIVISOR,
 					      STM32_PLLI2S_N_MULTIPLIER,
 					      STM32_PLLI2S_R_DIVISOR);
 		break;
 #endif /* STM32_SRC_PLLI2S_R */
+#if defined(STM32_SRC_PLLI2S_DIVR) && STM32_PLLI2S_R_ENABLED && STM32_PLLI2S_DIVR_ENABLED
+	case STM32_SRC_PLLI2S_DIVR:
+		*rate = get_pll_div_frequency(get_plli2ssrc_frequency(),
+					      STM32_PLLI2S_M_DIVISOR,
+					      STM32_PLLI2S_N_MULTIPLIER,
+					      STM32_PLLI2S_R_DIVISOR);
+		*rate /= STM32_PLLI2S_DIVR_DIVISOR;
+		break;
+#endif /* STM32_SRC_PLLI2S_DIVR */
 #if defined(STM32_SRC_PLLSAI_P) && STM32_PLLSAI_P_ENABLED
 	case STM32_SRC_PLLSAI_P:
 		*rate = get_pll_div_frequency(get_pllsaisrc_frequency(),
@@ -556,8 +619,7 @@ static int stm32_clock_control_get_subsys_rate(const struct device *clock,
 					      STM32_PLLSAI_Q_DIVISOR);
 		break;
 #endif /* STM32_SRC_PLLSAI_Q */
-#if defined(STM32_SRC_PLLSAI_DIVQ) && STM32_PLLSAI_Q_ENABLED && STM32_PLLSAI_DIVQ_ENABLED && \
-	defined(STM32_PLLSAI_DIVQ_DIVISOR)
+#if defined(STM32_SRC_PLLSAI_DIVQ) && STM32_PLLSAI_Q_ENABLED && STM32_PLLSAI_DIVQ_ENABLED
 	case STM32_SRC_PLLSAI_DIVQ:
 		*rate = get_pll_div_frequency(get_pllsaisrc_frequency(),
 					      STM32_PLLSAI_M_DIVISOR,
@@ -574,8 +636,7 @@ static int stm32_clock_control_get_subsys_rate(const struct device *clock,
 					      STM32_PLLSAI_R_DIVISOR);
 		break;
 #endif /* STM32_SRC_PLLSAI_R */
-#if defined(STM32_SRC_PLLSAI_DIVR) && STM32_PLLSAI_R_ENABLED && STM32_PLLSAI_DIVR_ENABLED && \
-	defined(STM32_PLLSAI_DIVR_DIVISOR)
+#if defined(STM32_SRC_PLLSAI_DIVR) && STM32_PLLSAI_R_ENABLED && STM32_PLLSAI_DIVR_ENABLED
 	case STM32_SRC_PLLSAI_DIVR:
 		*rate = get_pll_div_frequency(get_pllsaisrc_frequency(),
 					      STM32_PLLSAI_M_DIVISOR,

include/zephyr/drivers/clock_control/stm32_clock_control.h
@@ -179,9 +179,7 @@
 
 /** PLL node related symbols */
 
-#if DT_NODE_HAS_COMPAT_STATUS(DT_NODELABEL(pll), st_stm32f2_pll_clock, okay) || \
-	DT_NODE_HAS_COMPAT_STATUS(DT_NODELABEL(pll), st_stm32f4_pll_clock, okay)  || \
-	DT_NODE_HAS_COMPAT_STATUS(DT_NODELABEL(pll), st_stm32f7_pll_clock, okay)  || \
+#if DT_NODE_HAS_COMPAT_STATUS(DT_NODELABEL(pll), st_stm32fx_pll_clock, okay) || \
 	DT_NODE_HAS_COMPAT_STATUS(DT_NODELABEL(pll), st_stm32g0_pll_clock, okay)  || \
 	DT_NODE_HAS_COMPAT_STATUS(DT_NODELABEL(pll), st_stm32g4_pll_clock, okay)  || \
 	DT_NODE_HAS_COMPAT_STATUS(DT_NODELABEL(pll), st_stm32l4_pll_clock, okay)  || \
@@ -201,51 +199,43 @@
 #define STM32_PLL_Q_DIVISOR	DT_PROP_OR(DT_NODELABEL(pll), div_q, 1)
 #define STM32_PLL_R_ENABLED	DT_NODE_HAS_PROP(DT_NODELABEL(pll), div_r)
 #define STM32_PLL_R_DIVISOR	DT_PROP_OR(DT_NODELABEL(pll), div_r, 1)
+#define STM32_PLL_DIVR_ENABLED	DT_NODE_HAS_PROP(DT_NODELABEL(pll), div_divr)
+#define STM32_PLL_DIVR_DIVISOR	DT_PROP_OR(DT_NODELABEL(pll), div_divr, 1)
 #define STM32_PLL_S_ENABLED	DT_NODE_HAS_PROP(DT_NODELABEL(pll), div_s)
 #define STM32_PLL_S_DIVISOR	DT_PROP_OR(DT_NODELABEL(pll), div_s, 1)
 #define STM32_PLL_FRACN_ENABLED	DT_NODE_HAS_PROP(DT_NODELABEL(pll), fracn)
 #define STM32_PLL_FRACN_VALUE	DT_PROP_OR(DT_NODELABEL(pll), fracn, 1)
 #endif
 
-#if DT_NODE_HAS_COMPAT_STATUS(DT_NODELABEL(plli2s), st_stm32f4_plli2s_clock, okay)
-#define STM32_PLLI2S_ENABLED	1
-#define STM32_PLLI2S_M_DIVISOR		STM32_PLL_M_DIVISOR
-#define STM32_PLLI2S_N_MULTIPLIER	DT_PROP(DT_NODELABEL(plli2s), mul_n)
-#define STM32_PLLI2S_R_ENABLED		DT_NODE_HAS_PROP(DT_NODELABEL(plli2s), div_r)
-#define STM32_PLLI2S_R_DIVISOR		DT_PROP_OR(DT_NODELABEL(plli2s), div_r, 1)
-#endif
-
-#if DT_NODE_HAS_COMPAT_STATUS(DT_NODELABEL(plli2s), st_stm32f411_plli2s_clock, okay)
-#define STM32_PLLI2S_ENABLED	1
+#if DT_NODE_HAS_COMPAT_STATUS(DT_NODELABEL(plli2s), st_stm32fx_pll_clock, okay)
+#define STM32_PLLI2S_ENABLED		1
 #define STM32_PLLI2S_M_DIVISOR		DT_PROP(DT_NODELABEL(plli2s), div_m)
 #define STM32_PLLI2S_N_MULTIPLIER	DT_PROP(DT_NODELABEL(plli2s), mul_n)
+#define STM32_PLLI2S_P_ENABLED		DT_NODE_HAS_PROP(DT_NODELABEL(plli2s), div_p)
+#define STM32_PLLI2S_P_DIVISOR		DT_PROP_OR(DT_NODELABEL(plli2s), div_p, 1)
 #define STM32_PLLI2S_Q_ENABLED		DT_NODE_HAS_PROP(DT_NODELABEL(plli2s), div_q)
 #define STM32_PLLI2S_Q_DIVISOR		DT_PROP_OR(DT_NODELABEL(plli2s), div_q, 1)
+#define STM32_PLLI2S_DIVQ_ENABLED	DT_NODE_HAS_PROP(DT_NODELABEL(plli2s), div_divq)
+#define STM32_PLLI2S_DIVQ_DIVISOR	DT_PROP_OR(DT_NODELABEL(plli2s), div_divq, 1)
 #define STM32_PLLI2S_R_ENABLED		DT_NODE_HAS_PROP(DT_NODELABEL(plli2s), div_r)
 #define STM32_PLLI2S_R_DIVISOR		DT_PROP_OR(DT_NODELABEL(plli2s), div_r, 1)
+#define STM32_PLLI2S_DIVR_ENABLED	DT_NODE_HAS_PROP(DT_NODELABEL(plli2s), div_divr)
+#define STM32_PLLI2S_DIVR_DIVISOR	DT_PROP_OR(DT_NODELABEL(plli2s), div_divr, 1)
 #endif
 
-#if DT_NODE_HAS_COMPAT_STATUS(DT_NODELABEL(pllsai), st_stm32fx_pllsai_clock, okay)
-#define STM32_PLLSAI_ENABLED	1
+#if DT_NODE_HAS_COMPAT_STATUS(DT_NODELABEL(pllsai), st_stm32fx_pll_clock, okay)
+#define STM32_PLLSAI_ENABLED		1
 #define STM32_PLLSAI_M_DIVISOR		DT_PROP(DT_NODELABEL(pllsai), div_m)
 #define STM32_PLLSAI_N_MULTIPLIER	DT_PROP(DT_NODELABEL(pllsai), mul_n)
 #define STM32_PLLSAI_P_ENABLED		DT_NODE_HAS_PROP(DT_NODELABEL(pllsai), div_p)
 #define STM32_PLLSAI_P_DIVISOR		DT_PROP_OR(DT_NODELABEL(pllsai), div_p, 1)
 #define STM32_PLLSAI_Q_ENABLED		DT_NODE_HAS_PROP(DT_NODELABEL(pllsai), div_q)
-#define STM32_PLLSAI_DIVQ_ENABLED	DT_NODE_HAS_PROP(DT_NODELABEL(pllsai), div_divq)
-#if (STM32_PLLSAI_Q_ENABLED && !STM32_PLLSAI_DIVQ_ENABLED) || \
-	(!STM32_PLLSAI_Q_ENABLED && STM32_PLLSAI_DIVQ_ENABLED)
-#error "On STM32F4/STM32F7, both div_q and div_divq must be present if one of them is present"
-#endif
 #define STM32_PLLSAI_Q_DIVISOR		DT_PROP_OR(DT_NODELABEL(pllsai), div_q, 1)
+#define STM32_PLLSAI_DIVQ_ENABLED	DT_NODE_HAS_PROP(DT_NODELABEL(pllsai), div_divq)
 #define STM32_PLLSAI_DIVQ_DIVISOR	DT_PROP_OR(DT_NODELABEL(pllsai), div_divq, 1)
 #define STM32_PLLSAI_R_ENABLED		DT_NODE_HAS_PROP(DT_NODELABEL(pllsai), div_r)
-#define STM32_PLLSAI_DIVR_ENABLED	DT_NODE_HAS_PROP(DT_NODELABEL(pllsai), div_divr)
-#if (STM32_PLLSAI_R_ENABLED && !STM32_PLLSAI_DIVR_ENABLED) || \
-	(!STM32_PLLSAI_R_ENABLED && STM32_PLLSAI_DIVR_ENABLED)
-#error "On STM32F4/STM32F7, both div_r and div_divr must be present if one of them is present"
-#endif
 #define STM32_PLLSAI_R_DIVISOR		DT_PROP_OR(DT_NODELABEL(pllsai), div_r, 1)
+#define STM32_PLLSAI_DIVR_ENABLED	DT_NODE_HAS_PROP(DT_NODELABEL(pllsai), div_divr)
 #define STM32_PLLSAI_DIVR_DIVISOR	DT_PROP_OR(DT_NODELABEL(pllsai), div_divr, 1)
 #endif
 
@@ -464,6 +454,19 @@
 
 #endif
 
+/** PLLI2S clock source */
+#if DT_NODE_HAS_STATUS(DT_NODELABEL(plli2s), okay) && \
+	DT_NODE_HAS_PROP(DT_NODELABEL(plli2s), clocks)
+#define DT_PLLI2S_CLOCKS_CTRL	DT_CLOCKS_CTLR(DT_NODELABEL(plli2s))
+#if DT_SAME_NODE(DT_PLLI2S_CLOCKS_CTRL, DT_NODELABEL(clk_hsi))
+#define STM32_PLLI2S_SRC_HSI	1
+#endif
+#if DT_SAME_NODE(DT_PLLI2S_CLOCKS_CTRL, DT_NODELABEL(clk_hse))
+#define STM32_PLLI2S_SRC_HSE	1
+#endif
+
+#endif
+
 /** PLLSAI clock source */
 #if DT_NODE_HAS_STATUS(DT_NODELABEL(pllsai), okay) && \
 	DT_NODE_HAS_PROP(DT_NODELABEL(pllsai), clocks)
@@ -507,20 +510,6 @@
 #define STM32_PLLSAI2_SRC_HSE	1
 #endif
 
-#endif
-
-/* On STM32F4 series - PLL and PLLSAI share the same source */
-#if DT_NODE_HAS_COMPAT_STATUS(DT_NODELABEL(pll), st_stm32f4_pll_clock, okay) && \
-	DT_NODE_HAS_COMPAT_STATUS(DT_NODELABEL(pllsai), st_stm32fx_pllsai_clock, okay) && \
-	!DT_SAME_NODE(DT_PLL_CLOCKS_CTRL, DT_PLLSAI_CLOCKS_CTRL)
-#error "On STM32F4 series, PLL and PLLSAI must have the same source"
-#endif
-
-/* On STM32F7 series - PLL and PLLSAI share the same source */
-#if DT_NODE_HAS_COMPAT_STATUS(DT_NODELABEL(pll), st_stm32f7_pll_clock, okay) && \
-	DT_NODE_HAS_COMPAT_STATUS(DT_NODELABEL(pllsai), st_stm32fx_pllsai_clock, okay) && \
-	!DT_SAME_NODE(DT_PLL_CLOCKS_CTRL, DT_PLLSAI_CLOCKS_CTRL)
-#error "On STM32F7 series, PLL and PLLSAI must have the same source"
 #endif
 
 /* On STM32L4 series - PLL / PLLSAI1 and PLLSAI2 shared same source */

Infineon/zephyr-aurix

drivers/clock_control/clock_stm32_ll_common.c
@@ -222,6 +222,20 @@ int enabled_clock(uint32_t src_clk)
 		}
 		break;
 #endif /* STM32_SRC_PLL_R */
+#if defined(STM32_SRC_PLL_POST_R)
+	case STM32_SRC_PLL_POST_R:
+		if (!IS_ENABLED(STM32_PLL_R_ENABLED)) {
+			r = -ENOTSUP;
+		}
+		break;
+#endif /* STM32_SRC_PLL_POST_R */
+#if defined(STM32_SRC_PLLI2S_P)
+	case STM32_SRC_PLLI2S_P:
+		if (!IS_ENABLED(STM32_PLLI2S_P_ENABLED)) {
+			r = -ENOTSUP;
+		}
+		break;
+#endif /* STM32_SRC_PLLI2S_P */
 #if defined(STM32_SRC_PLLI2S_Q)
 	case STM32_SRC_PLLI2S_Q:
 		if (!IS_ENABLED(STM32_PLLI2S_Q_ENABLED)) {
@@ -229,6 +243,13 @@ int enabled_clock(uint32_t src_clk)
 		}
 		break;
 #endif /* STM32_SRC_PLLI2S_Q */
+#if defined(STM32_SRC_PLLI2S_POST_Q)
+	case STM32_SRC_PLLI2S_POST_Q:
+		if (!IS_ENABLED(STM32_PLLI2S_Q_ENABLED)) {
+			r = -ENOTSUP;
+		}
+		break;
+#endif /* STM32_SRC_PLLI2S_POST_Q */
 #if defined(STM32_SRC_PLLI2S_R)
 	case STM32_SRC_PLLI2S_R:
 		if (!IS_ENABLED(STM32_PLLI2S_R_ENABLED)) {
@@ -236,6 +257,13 @@ int enabled_clock(uint32_t src_clk)
 		}
 		break;
 #endif /* STM32_SRC_PLLI2S_R */
+#if defined(STM32_SRC_PLLI2S_POST_R)
+	case STM32_SRC_PLLI2S_POST_R:
+		if (!IS_ENABLED(STM32_PLLI2S_R_ENABLED)) {
+			r = -ENOTSUP;
+		}
+		break;
+#endif /* STM32_SRC_PLLI2S_POST_R */
 #if defined(STM32_SRC_PLLSAI_P)
 	case STM32_SRC_PLLSAI_P:
 		if (!IS_ENABLED(STM32_PLLSAI_P_ENABLED)) {
@@ -250,13 +278,13 @@ int enabled_clock(uint32_t src_clk)
 		}
 		break;
 #endif /* STM32_SRC_PLLSAI_Q */
-#if defined(STM32_SRC_PLLSAI_DIVQ)
-	case STM32_SRC_PLLSAI_DIVQ:
+#if defined(STM32_SRC_PLLSAI_POST_Q)
+	case STM32_SRC_PLLSAI_POST_Q:
 		if (!IS_ENABLED(STM32_PLLSAI_Q_ENABLED)) {
 			r = -ENOTSUP;
 		}
 		break;
-#endif /* STM32_SRC_PLLSAI_DIVQ */
+#endif /* STM32_SRC_PLLSAI_POST_Q */
 #if defined(STM32_SRC_PLLSAI_R)
 	case STM32_SRC_PLLSAI_R:
 		if (!IS_ENABLED(STM32_PLLSAI_R_ENABLED)) {
@@ -264,13 +292,13 @@ int enabled_clock(uint32_t src_clk)
 		}
 		break;
 #endif /* STM32_SRC_PLLSAI_R */
-#if defined(STM32_SRC_PLLSAI_DIVR)
-	case STM32_SRC_PLLSAI_DIVR:
+#if defined(STM32_SRC_PLLSAI_POST_R)
+	case STM32_SRC_PLLSAI_POST_R:
 		if (!IS_ENABLED(STM32_PLLSAI_R_ENABLED)) {
 			r = -ENOTSUP;
 		}
 		break;
-#endif /* STM32_SRC_PLLSAI_DIVR */
+#endif /* STM32_SRC_PLLSAI_POST_R */
 #if defined(STM32_SRC_PLLSAI1_P)
 	case STM32_SRC_PLLSAI1_P:
 		if (!IS_ENABLED(STM32_PLLSAI1_P_ENABLED)) {
@@ -524,22 +552,57 @@ static int stm32_clock_control_get_subsys_rate(const struct device *clock,
 					      STM32_PLL_R_DIVISOR);
 		break;
 #endif
-#if defined(STM32_SRC_PLLI2S_Q) && STM32_PLLI2S_Q_ENABLED && STM32_PLLI2S_ENABLED
-	case STM32_SRC_PLLI2S_Q:
+#if defined(STM32_SRC_PLL_POST_R) && STM32_PLL_R_ENABLED && STM32_PLL_POST_R_ENABLED
+	case STM32_SRC_PLL_POST_R:
 		*rate = get_pll_div_frequency(get_pllsrc_frequency(),
+					      STM32_PLL_M_DIVISOR,
+					      STM32_PLL_N_MULTIPLIER,
+					      STM32_PLL_R_DIVISOR);
+		*rate /= STM32_PLL_POST_R_DIVISOR;
+		break;
+#endif
+#if defined(STM32_SRC_PLLI2S_P) && STM32_PLLI2S_P_ENABLED
+	case STM32_SRC_PLLI2S_P:
+		*rate = get_pll_div_frequency(get_plli2ssrc_frequency(),
+					      STM32_PLLI2S_M_DIVISOR,
+					      STM32_PLLI2S_N_MULTIPLIER,
+					      STM32_PLLI2S_P_DIVISOR);
+		break;
+#endif /* STM32_SRC_PLLI2S_P */
+#if defined(STM32_SRC_PLLI2S_Q) && STM32_PLLI2S_Q_ENABLED
+	case STM32_SRC_PLLI2S_Q:
+		*rate = get_pll_div_frequency(get_plli2ssrc_frequency(),
 					      STM32_PLLI2S_M_DIVISOR,
 					      STM32_PLLI2S_N_MULTIPLIER,
 					      STM32_PLLI2S_Q_DIVISOR);
 		break;
 #endif /* STM32_SRC_PLLI2S_Q */
-#if defined(STM32_SRC_PLLI2S_R) && STM32_PLLI2S_ENABLED
+#if defined(STM32_SRC_PLLI2S_POST_Q) && STM32_PLLI2S_Q_ENABLED && STM32_PLLI2S_POST_Q_ENABLED
+	case STM32_SRC_PLLI2S_POST_Q:
+		*rate = get_pll_div_frequency(get_plli2ssrc_frequency(),
+					      STM32_PLLI2S_M_DIVISOR,
+					      STM32_PLLI2S_N_MULTIPLIER,
+					      STM32_PLLI2S_Q_DIVISOR);
+		*rate /= STM32_PLLI2S_POST_Q_DIVISOR;
+		break;
+#endif /* STM32_SRC_PLLI2S_POST_Q */
+#if defined(STM32_SRC_PLLI2S_R) && STM32_PLLI2S_R_ENABLED
 	case STM32_SRC_PLLI2S_R:
-		*rate = get_pll_div_frequency(get_pllsrc_frequency(),
+		*rate = get_pll_div_frequency(get_plli2ssrc_frequency(),
 					      STM32_PLLI2S_M_DIVISOR,
 					      STM32_PLLI2S_N_MULTIPLIER,
 					      STM32_PLLI2S_R_DIVISOR);
 		break;
 #endif /* STM32_SRC_PLLI2S_R */
+#if defined(STM32_SRC_PLLI2S_POST_R) && STM32_PLLI2S_R_ENABLED && STM32_PLLI2S_POST_R_ENABLED
+	case STM32_SRC_PLLI2S_POST_R:
+		*rate = get_pll_div_frequency(get_plli2ssrc_frequency(),
+					      STM32_PLLI2S_M_DIVISOR,
+					      STM32_PLLI2S_N_MULTIPLIER,
+					      STM32_PLLI2S_R_DIVISOR);
+		*rate /= STM32_PLLI2S_POST_R_DIVISOR;
+		break;
+#endif /* STM32_SRC_PLLI2S_POST_R */
 #if defined(STM32_SRC_PLLSAI_P) && STM32_PLLSAI_P_ENABLED
 	case STM32_SRC_PLLSAI_P:
 		*rate = get_pll_div_frequency(get_pllsaisrc_frequency(),
@@ -556,16 +619,15 @@ static int stm32_clock_control_get_subsys_rate(const struct device *clock,
 					      STM32_PLLSAI_Q_DIVISOR);
 		break;
 #endif /* STM32_SRC_PLLSAI_Q */
-#if defined(STM32_SRC_PLLSAI_DIVQ) && STM32_PLLSAI_Q_ENABLED && STM32_PLLSAI_DIVQ_ENABLED && \
-	defined(STM32_PLLSAI_DIVQ_DIVISOR)
-	case STM32_SRC_PLLSAI_DIVQ:
+#if defined(STM32_SRC_PLLSAI_POST_Q) && STM32_PLLSAI_Q_ENABLED && STM32_PLLSAI_POST_Q_ENABLED
+	case STM32_SRC_PLLSAI_POST_Q:
 		*rate = get_pll_div_frequency(get_pllsaisrc_frequency(),
 					      STM32_PLLSAI_M_DIVISOR,
 					      STM32_PLLSAI_N_MULTIPLIER,
 					      STM32_PLLSAI_Q_DIVISOR);
-		*rate /= STM32_PLLSAI_DIVQ_DIVISOR;
+		*rate /= STM32_PLLSAI_POST_Q_DIVISOR;
 		break;
-#endif /* STM32_SRC_PLLSAI_DIVQ */
+#endif /* STM32_SRC_PLLSAI_POST_Q */
 #if defined(STM32_SRC_PLLSAI_R) && STM32_PLLSAI_R_ENABLED
 	case STM32_SRC_PLLSAI_R:
 		*rate = get_pll_div_frequency(get_pllsaisrc_frequency(),
@@ -574,16 +636,15 @@ static int stm32_clock_control_get_subsys_rate(const struct device *clock,
 					      STM32_PLLSAI_R_DIVISOR);
 		break;
 #endif /* STM32_SRC_PLLSAI_R */
-#if defined(STM32_SRC_PLLSAI_DIVR) && STM32_PLLSAI_R_ENABLED && STM32_PLLSAI_DIVR_ENABLED && \
-	defined(STM32_PLLSAI_DIVR_DIVISOR)
-	case STM32_SRC_PLLSAI_DIVR:
+#if defined(STM32_SRC_PLLSAI_POST_R) && STM32_PLLSAI_R_ENABLED && STM32_PLLSAI_POST_R_ENABLED
+	case STM32_SRC_PLLSAI_POST_R:
 		*rate = get_pll_div_frequency(get_pllsaisrc_frequency(),
 					      STM32_PLLSAI_M_DIVISOR,
 					      STM32_PLLSAI_N_MULTIPLIER,
 					      STM32_PLLSAI_R_DIVISOR);
-		*rate /= STM32_PLLSAI_DIVR_DIVISOR;
+		*rate /= STM32_PLLSAI_POST_R_DIVISOR;
 		break;
-#endif /* STM32_SRC_PLLSAI_DIVR */
+#endif /* STM32_SRC_PLLSAI_POST_R */
 #if defined(STM32_SRC_PLLSAI1_P) && STM32_PLLSAI1_P_ENABLED
 	case STM32_SRC_PLLSAI1_P:
 		*rate = get_pll_div_frequency(get_pllsai1src_frequency(),

include/zephyr/drivers/clock_control/stm32_clock_control.h
@@ -179,9 +179,7 @@
 
 /** PLL node related symbols */
 
-#if DT_NODE_HAS_COMPAT_STATUS(DT_NODELABEL(pll), st_stm32f2_pll_clock, okay) || \
-	DT_NODE_HAS_COMPAT_STATUS(DT_NODELABEL(pll), st_stm32f4_pll_clock, okay)  || \
-	DT_NODE_HAS_COMPAT_STATUS(DT_NODELABEL(pll), st_stm32f7_pll_clock, okay)  || \
+#if DT_NODE_HAS_COMPAT_STATUS(DT_NODELABEL(pll), st_stm32fx_pll_clock, okay) || \
 	DT_NODE_HAS_COMPAT_STATUS(DT_NODELABEL(pll), st_stm32g0_pll_clock, okay)  || \
 	DT_NODE_HAS_COMPAT_STATUS(DT_NODELABEL(pll), st_stm32g4_pll_clock, okay)  || \
 	DT_NODE_HAS_COMPAT_STATUS(DT_NODELABEL(pll), st_stm32l4_pll_clock, okay)  || \
@@ -201,52 +199,44 @@
 #define STM32_PLL_Q_DIVISOR	DT_PROP_OR(DT_NODELABEL(pll), div_q, 1)
 #define STM32_PLL_R_ENABLED	DT_NODE_HAS_PROP(DT_NODELABEL(pll), div_r)
 #define STM32_PLL_R_DIVISOR	DT_PROP_OR(DT_NODELABEL(pll), div_r, 1)
+#define STM32_PLL_POST_R_ENABLED	DT_NODE_HAS_PROP(DT_NODELABEL(pll), post_div_r)
+#define STM32_PLL_POST_R_DIVISOR	DT_PROP_OR(DT_NODELABEL(pll), post_div_r, 1)
 #define STM32_PLL_S_ENABLED	DT_NODE_HAS_PROP(DT_NODELABEL(pll), div_s)
 #define STM32_PLL_S_DIVISOR	DT_PROP_OR(DT_NODELABEL(pll), div_s, 1)
 #define STM32_PLL_FRACN_ENABLED	DT_NODE_HAS_PROP(DT_NODELABEL(pll), fracn)
 #define STM32_PLL_FRACN_VALUE	DT_PROP_OR(DT_NODELABEL(pll), fracn, 0)
 #endif
 
-#if DT_NODE_HAS_COMPAT_STATUS(DT_NODELABEL(plli2s), st_stm32f4_plli2s_clock, okay)
-#define STM32_PLLI2S_ENABLED	1
-#define STM32_PLLI2S_M_DIVISOR		STM32_PLL_M_DIVISOR
-#define STM32_PLLI2S_N_MULTIPLIER	DT_PROP(DT_NODELABEL(plli2s), mul_n)
-#define STM32_PLLI2S_R_ENABLED		DT_NODE_HAS_PROP(DT_NODELABEL(plli2s), div_r)
-#define STM32_PLLI2S_R_DIVISOR		DT_PROP_OR(DT_NODELABEL(plli2s), div_r, 1)
-#endif
-
-#if DT_NODE_HAS_COMPAT_STATUS(DT_NODELABEL(plli2s), st_stm32f411_plli2s_clock, okay)
-#define STM32_PLLI2S_ENABLED	1
+#if DT_NODE_HAS_COMPAT_STATUS(DT_NODELABEL(plli2s), st_stm32fx_pll_clock, okay)
+#define STM32_PLLI2S_ENABLED		1
 #define STM32_PLLI2S_M_DIVISOR		DT_PROP(DT_NODELABEL(plli2s), div_m)
 #define STM32_PLLI2S_N_MULTIPLIER	DT_PROP(DT_NODELABEL(plli2s), mul_n)
+#define STM32_PLLI2S_P_ENABLED		DT_NODE_HAS_PROP(DT_NODELABEL(plli2s), div_p)
+#define STM32_PLLI2S_P_DIVISOR		DT_PROP_OR(DT_NODELABEL(plli2s), div_p, 1)
 #define STM32_PLLI2S_Q_ENABLED		DT_NODE_HAS_PROP(DT_NODELABEL(plli2s), div_q)
 #define STM32_PLLI2S_Q_DIVISOR		DT_PROP_OR(DT_NODELABEL(plli2s), div_q, 1)
+#define STM32_PLLI2S_POST_Q_ENABLED	DT_NODE_HAS_PROP(DT_NODELABEL(plli2s), post_div_q)
+#define STM32_PLLI2S_POST_Q_DIVISOR	DT_PROP_OR(DT_NODELABEL(plli2s), post_div_q, 1)
 #define STM32_PLLI2S_R_ENABLED		DT_NODE_HAS_PROP(DT_NODELABEL(plli2s), div_r)
 #define STM32_PLLI2S_R_DIVISOR		DT_PROP_OR(DT_NODELABEL(plli2s), div_r, 1)
+#define STM32_PLLI2S_POST_R_ENABLED	DT_NODE_HAS_PROP(DT_NODELABEL(plli2s), post_div_r)
+#define STM32_PLLI2S_POST_R_DIVISOR	DT_PROP_OR(DT_NODELABEL(plli2s), post_div_r, 1)
 #endif
 
-#if DT_NODE_HAS_COMPAT_STATUS(DT_NODELABEL(pllsai), st_stm32fx_pllsai_clock, okay)
-#define STM32_PLLSAI_ENABLED	1
+#if DT_NODE_HAS_COMPAT_STATUS(DT_NODELABEL(pllsai), st_stm32fx_pll_clock, okay)
+#define STM32_PLLSAI_ENABLED		1
 #define STM32_PLLSAI_M_DIVISOR		DT_PROP(DT_NODELABEL(pllsai), div_m)
 #define STM32_PLLSAI_N_MULTIPLIER	DT_PROP(DT_NODELABEL(pllsai), mul_n)
 #define STM32_PLLSAI_P_ENABLED		DT_NODE_HAS_PROP(DT_NODELABEL(pllsai), div_p)
 #define STM32_PLLSAI_P_DIVISOR		DT_PROP_OR(DT_NODELABEL(pllsai), div_p, 1)
 #define STM32_PLLSAI_Q_ENABLED		DT_NODE_HAS_PROP(DT_NODELABEL(pllsai), div_q)
-#define STM32_PLLSAI_DIVQ_ENABLED	DT_NODE_HAS_PROP(DT_NODELABEL(pllsai), div_divq)
-#if (STM32_PLLSAI_Q_ENABLED && !STM32_PLLSAI_DIVQ_ENABLED) || \
-	(!STM32_PLLSAI_Q_ENABLED && STM32_PLLSAI_DIVQ_ENABLED)
-#error "On STM32F4/STM32F7, both div_q and div_divq must be present if one of them is present"
-#endif
 #define STM32_PLLSAI_Q_DIVISOR		DT_PROP_OR(DT_NODELABEL(pllsai), div_q, 1)
-#define STM32_PLLSAI_DIVQ_DIVISOR	DT_PROP_OR(DT_NODELABEL(pllsai), div_divq, 1)
+#define STM32_PLLSAI_POST_Q_ENABLED	DT_NODE_HAS_PROP(DT_NODELABEL(pllsai), post_div_q)
+#define STM32_PLLSAI_POST_Q_DIVISOR	DT_PROP_OR(DT_NODELABEL(pllsai), post_div_q, 1)
 #define STM32_PLLSAI_R_ENABLED		DT_NODE_HAS_PROP(DT_NODELABEL(pllsai), div_r)
-#define STM32_PLLSAI_DIVR_ENABLED	DT_NODE_HAS_PROP(DT_NODELABEL(pllsai), div_divr)
-#if (STM32_PLLSAI_R_ENABLED && !STM32_PLLSAI_DIVR_ENABLED) || \
-	(!STM32_PLLSAI_R_ENABLED && STM32_PLLSAI_DIVR_ENABLED)
-#error "On STM32F4/STM32F7, both div_r and div_divr must be present if one of them is present"
-#endif
 #define STM32_PLLSAI_R_DIVISOR		DT_PROP_OR(DT_NODELABEL(pllsai), div_r, 1)
-#define STM32_PLLSAI_DIVR_DIVISOR	DT_PROP_OR(DT_NODELABEL(pllsai), div_divr, 1)
+#define STM32_PLLSAI_POST_R_ENABLED	DT_NODE_HAS_PROP(DT_NODELABEL(pllsai), post_div_r)
+#define STM32_PLLSAI_POST_R_DIVISOR	DT_PROP_OR(DT_NODELABEL(pllsai), post_div_r, 1)
 #endif
 
 #if DT_NODE_HAS_COMPAT_STATUS(DT_NODELABEL(pllsai1), st_stm32l4_pllsai_clock, okay)
@@ -464,6 +454,19 @@
 
 #endif
 
+/** PLLI2S clock source */
+#if DT_NODE_HAS_STATUS(DT_NODELABEL(plli2s), okay) && \
+	DT_NODE_HAS_PROP(DT_NODELABEL(plli2s), clocks)
+#define DT_PLLI2S_CLOCKS_CTRL	DT_CLOCKS_CTLR(DT_NODELABEL(plli2s))
+#if DT_SAME_NODE(DT_PLLI2S_CLOCKS_CTRL, DT_NODELABEL(clk_hsi))
+#define STM32_PLLI2S_SRC_HSI	1
+#endif
+#if DT_SAME_NODE(DT_PLLI2S_CLOCKS_CTRL, DT_NODELABEL(clk_hse))
+#define STM32_PLLI2S_SRC_HSE	1
+#endif
+
+#endif
+
 /** PLLSAI clock source */
 #if DT_NODE_HAS_STATUS(DT_NODELABEL(pllsai), okay) && \
 	DT_NODE_HAS_PROP(DT_NODELABEL(pllsai), clocks)
@@ -507,20 +510,6 @@
 #define STM32_PLLSAI2_SRC_HSE	1
 #endif
 
-#endif
-
-/* On STM32F4 series - PLL and PLLSAI share the same source */
-#if DT_NODE_HAS_COMPAT_STATUS(DT_NODELABEL(pll), st_stm32f4_pll_clock, okay) && \
-	DT_NODE_HAS_COMPAT_STATUS(DT_NODELABEL(pllsai), st_stm32fx_pllsai_clock, okay) && \
-	!DT_SAME_NODE(DT_PLL_CLOCKS_CTRL, DT_PLLSAI_CLOCKS_CTRL)
-#error "On STM32F4 series, PLL and PLLSAI must have the same source"
-#endif
-
-/* On STM32F7 series - PLL and PLLSAI share the same source */
-#if DT_NODE_HAS_COMPAT_STATUS(DT_NODELABEL(pll), st_stm32f7_pll_clock, okay) && \
-	DT_NODE_HAS_COMPAT_STATUS(DT_NODELABEL(pllsai), st_stm32fx_pllsai_clock, okay) && \
-	!DT_SAME_NODE(DT_PLL_CLOCKS_CTRL, DT_PLLSAI_CLOCKS_CTRL)
-#error "On STM32F7 series, PLL and PLLSAI must have the same source"
 #endif
 
 /* On STM32L4 series - PLL / PLLSAI1 and PLLSAI2 shared same source */