Infineon/zephyr-aurix

commit 8741c2d106fb7c43b6447b485112b6be06e86daf

samples: video: tcpserversink: check video_enqueue/dequeue return values
Add proper check of the return value of video_enqueue / video_dequeue.

Signed-off-by: Alain Volmat <alain.volmat@foss.st.com>

samples/drivers/video/tcpserversink/src/main.c
@@ -131,7 +131,10 @@ int configure_encoder(void)
 	}
 
 	buffer->type = VIDEO_BUF_TYPE_OUTPUT;
-	video_enqueue(encoder_dev, buffer);
+	if (video_enqueue(encoder_dev, buffer)) {
+		LOG_ERR("Unable to enqueue encoder output buf");
+		return -1;
+	}
 
 	/* Set input format */
 	if (strcmp(CONFIG_VIDEO_PIXEL_FORMAT, "")) {
@@ -165,12 +168,16 @@ int encode_frame(struct video_buffer *in, struct video_buffer **out)
 	int ret;
 
 	in->type = VIDEO_BUF_TYPE_INPUT;
-	video_enqueue(encoder_dev, in);
+	ret = video_enqueue(encoder_dev, in);
+	if (ret) {
+		LOG_ERR("Unable to enqueue encoder input buf");
+		return ret;
+	}
 
 	(*out)->type = VIDEO_BUF_TYPE_OUTPUT;
 	ret = video_dequeue(encoder_dev, out, K_FOREVER);
 	if (ret) {
-		LOG_ERR("Unable to dequeue encoder buf");
+		LOG_ERR("Unable to dequeue encoder output buf");
 		return ret;
 	}
 
@@ -422,7 +429,11 @@ int main(void)
 
 		/* Enqueue Buffers */
 		for (i = 0; i < ARRAY_SIZE(buffers); i++) {
-			video_enqueue(video_dev, buffers[i]);
+			ret = video_enqueue(video_dev, buffers[i]);
+			if (ret) {
+				LOG_ERR("Unable to enqueue video buf");
+				return 0;
+			}
 		}
 
 		/* Start video capture */
@@ -452,7 +463,12 @@ int main(void)
 			ret = sendall(client, vbuf_out->buffer, vbuf_out->bytesused);
 
 			vbuf_out->type = VIDEO_BUF_TYPE_OUTPUT;
-			video_enqueue(encoder_dev, vbuf_out);
+			ret = video_enqueue(encoder_dev, vbuf_out);
+			if (ret) {
+				LOG_ERR("Unable to enqueue encoder output buf");
+				return 0;
+			}
+
 #else
 			LOG_INF("Sending frame %d", i++);
 			/* Send video buffer to TCP client */
@@ -465,7 +481,11 @@ int main(void)
 			}
 
 			vbuf->type = VIDEO_BUF_TYPE_INPUT;
-			(void)video_enqueue(video_dev, vbuf);
+			ret = video_enqueue(video_dev, vbuf);
+			if (ret) {
+				LOG_ERR("Unable to enqueue video buf");
+				return 0;
+			}
 		} while (!ret);
 
 		/* stop capture */

commit 4afc05146cf568cdf1b80f107382f4c3b2d529fa

samples: video: tcpserversink: fix multiple connect/disconnect
Fix multiple connect/disconnect regression introduced in
8741c2d1 samples: video: tcpserversink: check video_enqueue/dequeue
return values.

Signed-off-by: Hugues Fruchet <hugues.fruchet@foss.st.com>

samples/drivers/video/tcpserversink/src/main.c
@@ -422,6 +422,8 @@ int main(void)
 
 	/* Connection loop */
 	do {
+		bool disconnected = false;
+
 		LOG_INF("TCP: Waiting for client...");
 
 		client = accept(sock, (struct sockaddr *)&client_addr, &client_addr_len);
@@ -473,6 +475,7 @@ int main(void)
 				vbuf_out->bytesused);
 			/* Send compressed video buffer to TCP client */
 			ret = sendall(client, vbuf_out->buffer, vbuf_out->bytesused);
+			disconnected = ret && ret != -EAGAIN;
 
 			vbuf_out->type = VIDEO_BUF_TYPE_OUTPUT;
 			ret = video_enqueue(encoder_dev, vbuf_out);
@@ -485,8 +488,9 @@ int main(void)
 			LOG_INF("Sending frame %d", i++);
 			/* Send video buffer to TCP client */
 			ret = sendall(client, vbuf->buffer, vbuf->bytesused);
+			disconnected = ret && ret != -EAGAIN;
 #endif
-			if (ret && ret != -EAGAIN) {
+			if (disconnected) {
 				/* client disconnected */
 				LOG_ERR("TCP: Client disconnected %d", ret);
 				close(client);
@@ -498,7 +502,7 @@ int main(void)
 				LOG_ERR("Unable to enqueue video buf");
 				return 0;
 			}
-		} while (!ret);
+		} while (!ret && !disconnected);
 
 		/* stop capture */
 		if (video_stream_stop(video_dev, type)) {