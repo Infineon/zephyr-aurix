zephyrproject-rtos/zephyr

Bluetooth: Host: Conn: Add GAP service validation
Implement the validation if there is exactly one GAP service
in GATT database.
The validation is performed as soon as the GATT database
is available and forbids to enable the Bluetooth if the configuration
is not compatible with the Bluetooth Specification.
The verification may be disabled by the user for better performance
or deliberate implementation that is not compliant with the
specification.

Signed-off-by: Radosław Koppel <r.koppel@k-el.com>

subsys/bluetooth/host/gatt_gap_svc_validate.h
@@ -1,0 +1,27 @@
+/** @file
+ *  @brief Internal API GATT GAP service validation.
+ */
+
+/*
+ * Copyright (c) 2025 Koppel Electronic
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef __BT_GATT_GAP_SVC_VALIDATE_H
+#define __BT_GATT_GAP_SVC_VALIDATE_H
+
+
+/** @brief Validate GATT database contains exactly one GAP service.
+ *
+ * This function iterates through the GATT attribute database and verifies
+ * that exactly one Generic Access Profile (GAP) service is present. The
+ * Bluetooth specification requires that a GATT server expose exactly one
+ * GAP service.
+ *
+ * @return 0 on success (exactly one GAP service found).
+ * @return -EINVAL if zero or multiple GAP services are found.
+ */
+int gatt_gap_svc_validate(void);
+
+#endif /* __BT_GATT_GAP_SVC_VALIDATE_H */

Infineon/zephyr-aurix

subsys/bluetooth/host/gatt_gap_svc_validate.h
@@ -1,0 +1,27 @@
+/** @file
+ *  @brief Internal API GATT GAP service validation.
+ */
+
+/*
+ * Copyright (c) 2025 Koppel Electronic
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef ZEPHYR_SUBSYS_BLUETOOTH_HOST_GATT_GAP_SVC_VALIDATE_H
+#define ZEPHYR_SUBSYS_BLUETOOTH_HOST_GATT_GAP_SVC_VALIDATE_H
+
+
+/** @brief Validate GATT database contains exactly one GAP service.
+ *
+ * This function iterates through the GATT attribute database and verifies
+ * that exactly one Generic Access Profile (GAP) service is present. The
+ * Bluetooth specification requires that a GATT server expose exactly one
+ * GAP service.
+ *
+ * @return 0 on success (exactly one GAP service found).
+ * @return -EINVAL if zero or multiple GAP services are found.
+ */
+int gatt_gap_svc_validate(void);
+
+#endif /* ZEPHYR_SUBSYS_BLUETOOTH_HOST_GATT_GAP_SVC_VALIDATE_H */