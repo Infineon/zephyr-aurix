Infineon/zephyr-aurix

commit 95513232aaa45c85574d3956de512ace4ea4f05c

Bluetooth: Classic: Fix initialization to support re-initialization
In current implementation, the Classic L2CAP server list will be
cleared when the function `bt_enable()` called. It causes the
registered Classic L2CAP servers are unregistered. However, the
higher-ups were completely unaware of this behavior. It causes the
Classic L2CAP server cannot work after executing the sequence
`bt_enable()`, `bt_disable()`, and `bt_enable()`. Also this behavior
is inconsistent with LE L2CAP server.

Remove the initialization of Classic L2CAP server list from function
`bt_l2cap_br_init()` to fix the issue.

Make Bluetooth Classic profile initialization functions idempotent
by adding static initialized flags to prevent re-initialization.
Change return type from int to void since errors are now logged
but not propagated.

Changes include:
- Add initialized flag to prevent multiple initialization
- Change return type to void for init functions
- Mark err variables as __maybe_unused where appropriate
- Improve error logging with error codes
- Handle -EEXIST and -EALREADY errors for re-registration
- Initialize connection pools before checking initialized flag
  in AVRCP to support re-initialization scenarios
- Remove unnecessary sys_slist_init in bt_l2cap_br_init

Signed-off-by: Lyle Zhu <lyle.zhu@nxp.com>

subsys/bluetooth/host/classic/did.c
@@ -65,7 +65,21 @@ static struct bt_sdp_attribute did_attrs[] = {
 
 static struct bt_sdp_record did_rec = BT_SDP_RECORD(did_attrs);
 
-int bt_did_init(void)
+void bt_did_init(void)
 {
-	return bt_sdp_register_service(&did_rec);
+	__maybe_unused int err;
+
+	static bool initialized;
+
+	if (initialized) {
+		return;
+	}
+
+	err = bt_sdp_register_service(&did_rec);
+	if (err != 0) {
+		LOG_ERR("Failed to register DID SDP record (err %d)", err);
+		return;
+	}
+
+	initialized = true;
 }

subsys/bluetooth/host/classic/a2dp.c
@@ -4,7 +4,7 @@
 
 /*
  * Copyright (c) 2015-2016 Intel Corporation
- * Copyright 2021,2024 NXP
+ * Copyright 2021,2024-2025 NXP
  *
  * SPDX-License-Identifier: Apache-2.0
  */
@@ -1463,23 +1463,25 @@ static struct bt_avdtp_event_cb avdtp_cb = {
 	.accept = a2dp_accept,
 };
 
-int bt_a2dp_init(void)
+void bt_a2dp_init(void)
 {
-	int err;
+	__maybe_unused int err;
+
+	static bool initialized;
+
+	if (initialized) {
+		return;
+	}
 
 	/* Register event handlers with AVDTP */
 	err = bt_avdtp_register(&avdtp_cb);
-	if (err < 0) {
-		LOG_ERR("A2DP registration failed");
-		return err;
-	}
-
-	ARRAY_FOR_EACH(connection, i) {
-		memset(&connection[i], 0, sizeof(struct bt_a2dp));
+	if ((err < 0) && (err != -EALREADY)) {
+		LOG_ERR("A2DP registration failed (err %d)", err);
+		return;
 	}
 
 	LOG_DBG("A2DP Initialized successfully.");
-	return 0;
+	initialized = true;
 }
 
 struct bt_a2dp *bt_a2dp_connect(struct bt_conn *conn)

subsys/bluetooth/host/classic/a2dp_internal.h
@@ -4,9 +4,10 @@
 
 /*
  * Copyright (c) 2015-2016 Intel Corporation
+ * Copyright 2025 NXP
  *
  * SPDX-License-Identifier: Apache-2.0
  */
 
 /* To be called when first SEP is being registered */
-int bt_a2dp_init(void);
+void bt_a2dp_init(void);

subsys/bluetooth/host/classic/avctp.c
@@ -5,6 +5,7 @@
 /*
  * Copyright (c) 2015-2016 Intel Corporation
  * Copyright (C) 2024 Xiaomi Corporation
+ * Copyright 2025 NXP
  *
  * SPDX-License-Identifier: Apache-2.0
  */
@@ -718,12 +719,18 @@ int bt_avctp_server_register(struct bt_avctp_server *server)
 	return err;
 }
 
-int bt_avctp_init(void)
+void bt_avctp_init(void)
 {
+	static bool initialized;
+
+	if (initialized) {
+		return;
+	}
+
+	initialized = true;
+
 	LOG_DBG("Initializing AVCTP");
 	/* Locking semaphore initialized to 1 (unlocked) */
 	k_sem_init(&avctp_lock, 1, 1);
 	k_work_init_delayable(&avctp_tx_work, avctp_tx_processor);
-
-	return 0;
 }

subsys/bluetooth/host/classic/avctp_internal.h
@@ -5,6 +5,7 @@
 /*
  * Copyright (c) 2015-2016 Intel Corporation
  * Copyright (C) 2024 Xiaomi Corporation
+ * Copyright 2025 NXP
  *
  * SPDX-License-Identifier: Apache-2.0
  */
@@ -158,7 +159,7 @@ struct bt_avctp_event_cb {
 };
 
 /* Initialize AVCTP layer*/
-int bt_avctp_init(void);
+void bt_avctp_init(void);
 
 /* Application register with AVCTP layer */
 int bt_avctp_server_register(struct bt_avctp_server *server);

subsys/bluetooth/host/classic/avdtp.c
@@ -1,7 +1,7 @@
 /*
  * Audio Video Distribution Protocol
  *
- * Copyright 2024 - 2025 NXP
+ * Copyright 2024-2025 NXP
  *
  * SPDX-License-Identifier: Apache-2.0
  *
@@ -2233,8 +2233,12 @@ int bt_avdtp_register(struct bt_avdtp_event_cb *cb)
 {
 	LOG_DBG("");
 
-	if (event_cb) {
+	if (event_cb == cb) {
 		return -EALREADY;
+	}
+
+	if (event_cb != NULL) {
+		return -EEXIST;
 	}
 
 	event_cb = cb;
@@ -2280,9 +2284,11 @@ int bt_avdtp_register_sep(uint8_t media_type, uint8_t sep_type, struct bt_avdtp_
 }
 
 /* init function */
-int bt_avdtp_init(void)
+void bt_avdtp_init(void)
 {
 	int err;
+
+	static bool initialized;
 	static struct bt_l2cap_server avdtp_l2cap = {
 		.psm = BT_L2CAP_PSM_AVDTP,
 		.sec_level = BT_SECURITY_L2,
@@ -2291,13 +2297,18 @@ int bt_avdtp_init(void)
 
 	LOG_DBG("");
 
-	/* Register AVDTP PSM with L2CAP */
-	err = bt_l2cap_br_server_register(&avdtp_l2cap);
-	if (err < 0) {
-		LOG_ERR("AVDTP L2CAP Registration failed %d", err);
+	if (initialized) {
+		return;
 	}
 
-	return err;
+	/* Register AVDTP PSM with L2CAP */
+	err = bt_l2cap_br_server_register(&avdtp_l2cap);
+	if ((err < 0) && (err != -EEXIST)) {
+		LOG_ERR("AVDTP L2CAP Registration failed %d", err);
+		return;
+	}
+
+	initialized = true;
 }
 
 /* AVDTP Discover Request */

subsys/bluetooth/host/classic/avdtp_internal.h
@@ -2,7 +2,7 @@
  * avdtp_internal.h - avdtp handling
 
  * Copyright (c) 2015-2016 Intel Corporation
- * Copyright 2021,2024 NXP
+ * Copyright 2021,2024-2025 NXP
  *
  * SPDX-License-Identifier: Apache-2.0
  */
@@ -279,7 +279,7 @@ struct bt_avdtp_event_cb {
 };
 
 /* Initialize AVDTP layer*/
-int bt_avdtp_init(void);
+void bt_avdtp_init(void);
 
 /* Application register with AVDTP layer */
 int bt_avdtp_register(struct bt_avdtp_event_cb *cb);

subsys/bluetooth/host/classic/avrcp.c
@@ -5,6 +5,7 @@
 /*
  * Copyright (c) 2015-2016 Intel Corporation
  * Copyright (C) 2024 Xiaomi Corporation
+ * Copyright 2025 NXP
  *
  * SPDX-License-Identifier: Apache-2.0
  */
@@ -2935,17 +2936,44 @@ static int avrcp_browsing_accept(struct bt_conn *conn, struct bt_avctp **session
 }
 #endif /* CONFIG_BT_AVRCP_BROWSING */
 
-int bt_avrcp_init(void)
+void bt_avrcp_init(void)
 {
-	int err;
+	__maybe_unused int err;
+
+	static bool initialized;
+
+	/* Init CT and TG connection pool*/
+	__ASSERT(ARRAY_SIZE(bt_avrcp_ct_pool) == ARRAY_SIZE(avrcp_connection), "CT size mismatch");
+	__ASSERT(ARRAY_SIZE(bt_avrcp_tg_pool) == ARRAY_SIZE(avrcp_connection), "TG size mismatch");
+
+	ARRAY_FOR_EACH(avrcp_connection, i) {
+		bt_avrcp_ct_pool[i].avrcp = &avrcp_connection[i];
+		bt_avrcp_tg_pool[i].avrcp = &avrcp_connection[i];
+
+		if (!initialized) {
+			/* Init delay work */
+			k_work_init_delayable(&bt_avrcp_tg_pool[i].vd_rsp_tx_work,
+					      bt_avrcp_tg_vendor_tx_work);
+			sys_slist_init(&bt_avrcp_tg_pool[i].vd_rsp_tx_pending);
+
+			k_sem_init(&bt_avrcp_tg_pool[i].lock, 1, 1);
+		}
+
+		memset(bt_avrcp_ct_pool[i].ct_notify, 0, sizeof(bt_avrcp_ct_pool[i].ct_notify));
+		memset(bt_avrcp_tg_pool[i].tg_notify, 0, sizeof(bt_avrcp_tg_pool[i].tg_notify));
+	}
+
+	if (initialized) {
+		return;
+	}
 
 	/* Register event handlers with AVCTP */
 	avctp_server.l2cap.psm = BT_L2CAP_PSM_AVRCP;
 	avctp_server.accept = avrcp_accept;
 	err = bt_avctp_server_register(&avctp_server);
 	if (err < 0) {
-		LOG_ERR("AVRCP registration failed");
-		return err;
+		LOG_ERR("AVRCP registration failed (err %d)", err);
+		return;
 	}
 
 #if defined(CONFIG_BT_AVRCP_BROWSING)
@@ -2953,16 +2981,16 @@ int bt_avrcp_init(void)
 	avctp_browsing_server.accept = avrcp_browsing_accept;
 	err = bt_avctp_server_register(&avctp_browsing_server);
 	if (err < 0) {
-		LOG_ERR("AVRCP browsing registration failed");
-		return err;
+		LOG_ERR("AVRCP browsing registration failed (err %d)", err);
+		return;
 	}
 #endif /* CONFIG_BT_AVRCP_BROWSING */
 
 #if defined(CONFIG_BT_AVRCP_TG_COVER_ART)
 	err = bt_avrcp_tg_cover_art_init(&bt_avrcp_tg_cover_art_psm);
 	if (err < 0) {
-		LOG_ERR("AVRCP Cover Art initialization failed");
-		return err;
+		LOG_ERR("AVRCP Cover Art initialization failed (err %d)", err);
+		return;
 	}
 #endif /* CONFIG_BT_AVRCP_TG_COVER_ART */
 
@@ -2974,26 +3002,9 @@ int bt_avrcp_init(void)
 	bt_sdp_register_service(&avrcp_ct_rec);
 #endif /* CONFIG_BT_AVRCP_CONTROLLER */
 
-	/* Init CT and TG connection pool*/
-	__ASSERT(ARRAY_SIZE(bt_avrcp_ct_pool) == ARRAY_SIZE(avrcp_connection), "CT size mismatch");
-	__ASSERT(ARRAY_SIZE(bt_avrcp_tg_pool) == ARRAY_SIZE(avrcp_connection), "TG size mismatch");
-
-	ARRAY_FOR_EACH(avrcp_connection, i) {
-		bt_avrcp_ct_pool[i].avrcp = &avrcp_connection[i];
-		bt_avrcp_tg_pool[i].avrcp = &avrcp_connection[i];
-		/* Init delay work */
-		k_work_init_delayable(&bt_avrcp_tg_pool[i].vd_rsp_tx_work,
-				      bt_avrcp_tg_vendor_tx_work);
-		sys_slist_init(&bt_avrcp_tg_pool[i].vd_rsp_tx_pending);
-
-		k_sem_init(&bt_avrcp_tg_pool[i].lock, 1, 1);
-
-		memset(bt_avrcp_ct_pool[i].ct_notify, 0, sizeof(bt_avrcp_ct_pool[i].ct_notify));
-
-		memset(bt_avrcp_tg_pool[i].tg_notify, 0, sizeof(bt_avrcp_tg_pool[i].tg_notify));
-	}
 	LOG_DBG("AVRCP Initialized successfully.");
-	return 0;
+
+	initialized = true;
 }
 
 int bt_avrcp_connect(struct bt_conn *conn)

commit ed328399cca1529ff90e8e323e4afd82965547bb

bluetooth: classic: did: Set up logging
This file fails to compile since commit 95513232aaa4 ("Bluetooth:
Classic: Fix initialization to support re-initialization"), which added
a LOG_ERR() invocation but didn't setup logging for the file.

Fixes #101810

Signed-off-by: Thomas Hebb <tommyhebb@gmail.com>

subsys/bluetooth/host/classic/did.c
@@ -11,6 +11,9 @@
 
 #include "did_internal.h"
 
+#include <zephyr/logging/log.h>
+LOG_MODULE_REGISTER(bt_did);
+
 #define DID_VER_1_3 (0x0103u)
 
 #define BLUETOOTH_DEVICE_IDENTIFY_SPEC_VERSION 0x0103