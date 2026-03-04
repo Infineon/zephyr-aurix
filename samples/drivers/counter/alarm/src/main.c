zephyrproject-rtos/zephyr

samples: drivers: counter: Add support for RZ/A2M-EVK
Add support for RZ/A2M-EVK to Counter driver sample

Signed-off-by: Hieu Nguyen <hieu.nguyen.ym@bp.renesas.com>
Signed-off-by: Tien Nguyen <tien.nguyen.zg@renesas.com>

samples/drivers/counter/alarm/src/main.c
@@ -78,6 +78,8 @@ struct counter_alarm_cfg alarm_cfg;
 #define TIMER DT_NODELABEL(rtc)
 #elif defined(CONFIG_COUNTER_MCUX_RTC)
 #define TIMER DT_NODELABEL(rtc)
+#elif defined(CONFIG_COUNTER_RENESAS_RZA2M_OSTM)
+#define TIMER DT_INST(0, renesas_rza2m_ostm_counter)
 #else
 #error Unable to find a counter device node in devicetree
 #endif

Infineon/zephyr-aurix

samples/drivers/counter/alarm/src/main.c
@@ -82,6 +82,8 @@ struct counter_alarm_cfg alarm_cfg;
 #define SAMPLE_TIMER DT_NODELABEL(rtc)
 #elif defined(CONFIG_COUNTER_MCUX_RTC)
 #define SAMPLE_TIMER DT_NODELABEL(rtc)
+#elif defined(CONFIG_COUNTER_RENESAS_RZA2M_OSTM)
+#define SAMPLE_TIMER DT_INST(0, renesas_rza2m_ostm_counter)
 #else
 #error Unable to find a counter device node in devicetree
 #endif