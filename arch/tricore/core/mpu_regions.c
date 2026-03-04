c2843e2f25905e726a77bc2377b23dfe7131dec7
Christoph Seitz <christoph.seitz@infineon.com>
2026-03-04 22:36:31 +0100

    arch: tricore: Rework MPU region definition
    This commits introduces a global region configuration and
    uses this plus the device tree information to initialize the static
    MPU configuration during initialization.
    
    Signed-off-by: Christoph Seitz <christoph.seitz@infineon.com>

---
+++ arch/tricore/core/mpu_regions.c
@@ -0,0 +1,32 @@
+/*
+ * Copyright (c) 2026, Infineon Technologies AG
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+#include <zephyr/arch/cpu.h>
+
+static const struct tricore_mpu_region sections[] = {
+	{
+		.name = "FLASH",
+		.start = CONFIG_FLASH_BASE_ADDRESS,
+		.end = CONFIG_FLASH_BASE_ADDRESS + CONFIG_FLASH_SIZE * 1024,
+		.flags = TRICORE_MPU_ACCESS_P_RX_U_RX,
+	},
+	{
+		.name = "SRAM",
+		.start = CONFIG_SRAM_BASE_ADDRESS,
+		.end = CONFIG_SRAM_BASE_ADDRESS + CONFIG_SRAM_SIZE * 1024,
+		.flags = TRICORE_MPU_ACCESS_P_RW_U_NA,
+	},
+	{
+		.name = "PERIPHERALS",
+		.start = 0xF0000000,
+		.end = 0xFFFFFFFF,
+		.flags = TRICORE_MPU_ACCESS_P_RW_U_NA,
+	},
+};
+
+const struct tricore_mpu_config mpu_config = {
+	.num_regions = ARRAY_SIZE(sections),
+	.regions = sections,
+};
