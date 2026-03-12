dd339693072f20c18ce7393d5516826dc1fcdb14
Christoph Seitz <christoph.seitz@infineon.com>
2026-03-12 19:37:48 +0100

    soc: infineon: tc4x: fix module kernel reset sequence
    The TC4x reset sequence uses RST_CTRLA/RST_CTRLB/RST_STAT at
    offsets 0x00/0x04/0x08. Fix the register offsets and clear the
    status before triggering reset. Poll RST_STAT for completion
    instead of RST_CTRLA.
    
    Signed-off-by: Christoph Seitz <christoph.seitz@infineon.com>

--- soc/infineon/aurix/tc4x/soc.h
+++ soc/infineon/aurix/tc4x/soc.h
@@ -32,10 +32,10 @@ static inline int aurix_disable_clock(uintptr_t clc, uint32_t timeout)
 
 static inline int aurix_kernel_reset(mem_addr_t krst0, uint32_t timeout)
 {
-	sys_write32(1, krst0 + 8);
+	sys_write32(BIT(31), krst0 + 4);
 	sys_write32(1, krst0);
 	sys_write32(1, krst0 + 4);
-	return WAIT_FOR((sys_read32(krst0) & 0x2) == 0x2, timeout, k_busy_wait(1));
+	return WAIT_FOR((sys_read32(krst0 + 8) & 0x1) == 0x1, timeout, k_busy_wait(1));
 }
 
 static inline void aurix_start_core(uint8_t core_id, uint32_t pc)
