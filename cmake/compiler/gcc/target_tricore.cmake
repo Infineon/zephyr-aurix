c659867ba7a5e745c4d3036b478d01942a9b2982
Christoph Seitz <christoph.seitz@infineon.com>
2026-03-12 19:26:35 +0100

    cmake: add TriCore toolchain and compiler configuration
    Add target cmake files for both clang and GCC compilers on TriCore,
    selecting the appropriate -mcpu flag based on TC1.6P or TC1.8P core
    variant. Set the LLVM target triple to tricore-unknown-elf and
    default to picolibc module build since the TriCore toolchain does
    not bundle picolibc.
    
    Signed-off-by: Christoph Seitz <christoph.seitz@infineon.com>

---
+++ cmake/compiler/gcc/target_tricore.cmake
@@ -0,0 +1,16 @@
+set(CMAKE_TRY_COMPILE_TARGET_TYPE STATIC_LIBRARY)
+
+if (CONFIG_CPU_TC162)
+    set(GCC_M_CPU tc39xx)
+elseif (CONFIG_CPU_TC18)
+    set(GCC_M_CPU tc4DAx)
+else()
+    message(FATAL_ERROR "Unsupported TriCore CPU")
+endif()
+
+set(TRICORE_C_FLAGS "-mcpu=${GCC_M_CPU}")
+list(APPEND TOOLCHAIN_C_FLAGS "-mno-eabi-bitfield-limit")
+list(APPEND TOOLCHAIN_C_FLAGS "-fstrict-volatile-bitfields")
+
+list(APPEND TOOLCHAIN_C_FLAGS ${TRICORE_C_FLAGS})
+list(APPEND TOOLCHAIN_LD_FLAGS NO_SPLIT ${TRICORE_C_FLAGS})
