zephyrproject-rtos/zephyr

soc: raspberrypi: rp2350: add west sign for secure boot
Wrap the picotool sign command (requires recent picotool) to build
signed ELF files (which can be later converted to UF2 as well).

The resulting files boot, yet this has not been tested with actual
Secure Boot yet.

Signed-off-by: Dmitrii Sharshakov <d3dx12.xx@gmail.com>

scripts/west_commands/sign.py
@@ -132,8 +132,8 @@ class Sign(Forceable):
 
         # general options
         group = parser.add_argument_group('tool control options')
-        group.add_argument('-t', '--tool', choices=['imgtool', 'rimage', 'silabs_commander'],
-                           help='''image signing tool name; imgtool, rimage and silabs_commander
+        group.add_argument('-t', '--tool', choices=['imgtool', 'rimage', 'silabs_commander', 'picotool'],
+                           help='''image signing tool name; imgtool, rimage, silabs_commander and picotool
                            are currently supported (imgtool is deprecated)''')
         group.add_argument('-p', '--tool-path', default=None,
                            help='''path to the tool itself, if needed''')
@@ -217,6 +217,8 @@ schema (rimage "target") is not defined in board.cmake.''')
             signer = RimageSigner()
         elif args.tool == 'silabs_commander':
             signer = CommanderSigner()
+        elif args.tool == 'picotool':
+            signer = PicotoolSigner()
         # (Add support for other signers here in elif blocks)
         else:
             if args.tool is None:
@@ -723,6 +725,30 @@ class CommanderSigner(Signer):
             commandline.extend(["--encrypt", encrypt_key])
         if sign_key:
             commandline.extend(["--sign", sign_key])
+        commandline.extend(command.args.tool_args)
+
+        if not command.args.quiet:
+            command.inf("Signing with:", ' '.join(commandline))
+        subprocess.run(commandline, check=True)
+
+class PicotoolSigner(CommonSigner):
+    @staticmethod
+    def get_input_output(command, build_dir, build_conf):
+        kernel_prefix = CommonSigner.get_basename(build_dir, build_conf)
+        in_file = f'{kernel_prefix}.elf'
+        out_file = command.args.sbin or f'{kernel_prefix}.signed.elf'
+        return (in_file, out_file)
+
+    def sign(self, command, build_dir, build_conf, formats):
+        tool = self.get_tool(command, 'picotool')
+        in_file, out_file = self.get_input_output(command, build_dir, build_conf)
+        key_file = getattr(command.args, 'key',
+                           build_conf.get('CONFIG_RP2350_SIGNING_KEY', None))
+
+        if not key_file:
+            command.die('Please provide a key file using RP2350_SIGNING_KEY Kconfig option')
+
+        commandline = [ tool, "seal", "--sign", in_file, out_file, key_file ]
         commandline.extend(command.args.tool_args)
 
         if not command.args.quiet:

Infineon/zephyr-aurix

scripts/west_commands/sign.py
@@ -124,8 +124,8 @@ class Sign(Forceable):
 
         # general options
         group = parser.add_argument_group('tool control options')
-        group.add_argument('-t', '--tool', choices=['imgtool', 'rimage', 'silabs_commander'],
-                           help='''image signing tool name; imgtool, rimage and silabs_commander
+        group.add_argument('-t', '--tool', choices=['imgtool', 'picotool', 'rimage', 'silabs_commander'],
+                           help='''image signing tool name; imgtool, picotool, rimage and silabs_commander
                            are currently supported (imgtool is deprecated)''')
         group.add_argument('-p', '--tool-path', default=None,
                            help='''path to the tool itself, if needed''')
@@ -203,6 +203,8 @@ schema (rimage "target") is not defined in board.cmake.''')
             if args.if_tool_available:
                 self.die('imgtool does not support --if-tool-available')
             signer = ImgtoolSigner()
+        elif args.tool == 'picotool':
+            signer = PicotoolSigner()
         elif args.tool == 'rimage':
             signer = RimageSigner()
         elif args.tool == 'silabs_commander':
@@ -708,6 +710,30 @@ class CommanderSigner(Signer):
             commandline.extend(["--encrypt", encrypt_key])
         if sign_key:
             commandline.extend(["--sign", sign_key])
+        commandline.extend(command.args.tool_args)
+
+        if not command.args.quiet:
+            command.inf("Signing with:", ' '.join(commandline))
+        subprocess.run(commandline, check=True)
+
+class PicotoolSigner(Signer):
+    @staticmethod
+    def get_input_output(command, build_dir, build_conf):
+        kernel_prefix = pathlib.Path(build_dir) / 'zephyr' / get_kernel_bin_name(build_conf)
+        in_file = f'{kernel_prefix}.elf'
+        out_file = command.args.sbin or f'{kernel_prefix}.signed.elf'
+        return (in_file, out_file)
+
+    def sign(self, command, build_dir, build_conf, formats):
+        tool = get_tool_path(command, 'picotool')
+        in_file, out_file = self.get_input_output(command, build_dir, build_conf)
+        key_file = getattr(command.args, 'key',
+                           build_conf.get('CONFIG_RPI_PICO_SIGNING_KEY', None))
+
+        if not key_file:
+            command.die('Please provide a key file using RPI_PICO_SIGNING_KEY Kconfig option')
+
+        commandline = [ tool, "seal", "--sign", in_file, out_file, key_file ]
         commandline.extend(command.args.tool_args)
 
         if not command.args.quiet: