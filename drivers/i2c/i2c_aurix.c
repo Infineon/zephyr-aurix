63a2a2e3df3158c715687a80dd06c1f1c1b6ca93
Parthiban Nallathambi <parthiban@linumiz.com>
2026-04-26 06:03:01 +0530

    drivers: can: add AURIX MCMCAN driver wrapping Bosch M_CAN
    AURIX wraps the Bosch M_CAN cores with extra glue for clock
    selection, MRAM windows and IRQ routing. A thin AURIX-specific
    wrapper around the upstream Bosch driver lets existing CAN
    samples target Infineon parts unchanged.
    
    Signed-off-by: Parthiban Nallathambi <parthiban@linumiz.com>

--- drivers/i2c/i2c_aurix.c
+++ drivers/i2c/i2c_aurix.c
@@ -72,20 +72,22 @@ static inline int i2c_aurix_set_baudrate(const struct device *dev, uint32_t baud
 		return ret;
 	}
 
-	if (baudrate > 400000) 
+	if (baudrate > 400000) // for High Speed mode
 	{
-		dec = DIV_ROUND_UP((((fi2c / baudrate) * 46) - 92), 5); 
-	} else 
+		dec = DIV_ROUND_UP((((fi2c / baudrate) * 46) - 92), 5); // always: Inc = 46
+	} else // for Standard and fast mode
 	{
-		dec = DIV_ROUND_UP((((fi2c / rmc) / baudrate) - 3), 2); 
+		dec = DIV_ROUND_UP((((fi2c / rmc) / baudrate) - 3), 2); // always: Inc = 1
 	}
 
+	// dec:inc must be at least 6
 	if (dec < 6) {
 		dec = 6;
 	} else if (dec > BIT(11) - 1) {
 		dec = BIT(11) - 1;
 	}
 
+	/* Baudrate configuration */
 	if (baudrate > 400000) {
 		cfg->base->FDIVCFG.B = (Ifx_I2C_FDIVCFG_Bits){.DEC = 0x1D2, .INC = 5};
 		cfg->base->FDIVHIGHCFG.B = (Ifx_I2C_FDIVHIGHCFG_Bits){.DEC = dec, .INC = 46};
@@ -228,14 +230,16 @@ static int i2c_aurix_transfer(const struct device *dev, struct i2c_msg *msgs, ui
 		data->offset = 0;
 
 		for (i = msg_idx; i < num_msgs; i++) {
-			
+			/* Stop on read messages */
 			if ((msgs[i].flags & I2C_MSG_RW_MASK) != data->rnw) {
 				msg_idx = i - 1;
 				break;
 			}
 
+			/* Sum up len */
 			data->bytes += msgs[i].len;
 
+			/* Stop on restart or stop flags*/
 			if (msgs[i].flags & (I2C_MSG_RESTART | I2C_MSG_STOP)) {
 				msg_idx = i;
 				break;
@@ -270,12 +274,14 @@ static int i2c_aurix_transfer(const struct device *dev, struct i2c_msg *msgs, ui
 
 		k_event_wait(&data->irq_event, 0xF, true, K_FOREVER);
 
+		/* Stop bus if requested */
 		if (msgs[msg_idx].flags & I2C_MSG_STOP) {
 			k_event_clear(&data->irq_event, 0x1);
 			cfg->base->ENDDCTRL.B.SETEND = 1;
 			k_event_wait(&data->irq_event, 0x1, false, K_FOREVER);
 		}
 
+		/* Check for protocol error condition */
 		if (k_event_clear(&data->irq_event, 0xF) & BIT(1)) {
 			ret = -EIO;
 			break;
@@ -283,7 +289,7 @@ static int i2c_aurix_transfer(const struct device *dev, struct i2c_msg *msgs, ui
 	}
 
 err:
-	
+	/* Stop bus if not stopped and error happend */
 	if (cfg->base->BUSSTAT.B.BS == I2C_STATUS_BUSYMASTER) {
 		k_event_clear(&data->irq_event, 0x1);
 		cfg->base->ENDDCTRL.B.SETEND = 1;
@@ -314,13 +320,17 @@ static int i2c_aurix_transfer_cb(const struct device *dev, struct i2c_msg *msgs,
 	ARG_UNUSED(userdata);
 	return -ENOSYS;
 }
-#endif 
+#endif /* CONFIG_I2C_CALLBACK */
 #if defined(CONFIG_I2C_RTIO) || defined(__DOXYGEN__)
 
+/**
+ * @i2c_api_iodev_submit
+ * @brief Callback API for submitting work to a I2C device with RTIO
+ */
 static void i2c_aurix_iodev_submit(const struct device *dev, struct rtio_iodev_sqe *iodev_sqe)
 {
 }
-#endif 
+#endif /* CONFIG_I2C_RTIO */
 
 static int i2c_aurix_recover_bus(const struct device *dev)
 {
@@ -365,6 +375,7 @@ static int i2c_aurix_init(const struct device *dev)
 	struct i2c_aurix_data *const data = dev->data;
 	int ret;
 
+	/* Enable Module */
 	if (!aurix_enable_clock((uintptr_t)&cfg->base->CLC, 1000)) {
 		return -EIO;
 	}
@@ -390,16 +401,16 @@ static int i2c_aurix_init(const struct device *dev)
 
 	cfg->base->ADDRCFG.B = (Ifx_I2C_ADDRCFG_Bits){.ADR = 0,
 						      .TBAM = 0,
-						      .GCE = 0, 
-						      .MCE = 0, 
+						      .GCE = 0, // TODO: General call enable
+						      .MCE = 0, // TOOD: Master Code enable
 						      .MNS = 1,
 						      .SONA = 1,
 						      .SOPE = 0};
 	cfg->base->FIFOCFG.B = (Ifx_I2C_FIFOCFG_Bits){
 		.RXBS = 2,
 		.TXBS = 2,
-		.RXFA = 2, 
-		.TXFA = 2, 
+		.RXFA = 2, /* TODO: 32bit alignment */
+		.TXFA = 2, /* TODO: 32bit alignment */
 		.TXFC = 1,
 		.RXFC = 1,
 		.CRBC = 0,
