570dc9bd598c6b86af931295c50376a12d02a56c
Parthiban Nallathambi <parthiban@linumiz.com>
2026-04-29 03:50:40 +0530

    drivers: gpio: tc4x: enable EGTM CMU clocks before arming pad IRQ
    Arming a pad interrupt before the EGTM CMU clock is up silently
    drops the configuration; subsequent presses never fire the IRQ.
    Bring the clock up first.
    
    Signed-off-by: Parthiban Nallathambi <parthiban@linumiz.com>

--- drivers/gpio/gpio_tc4x.c
+++ drivers/gpio/gpio_tc4x.c
@@ -22,25 +22,19 @@
 
 #define DT_DRV_COMPAT infineon_tc4x_gpio
 
-/**
- * @brief Common gpio flags to custom flags
- */
 static int gpio_tc4x_flags_to_drvcfg(gpio_flags_t flags, Ifx_P_PADCFG_DRVCFG *drvcfg)
 {
 	bool is_input = flags & GPIO_INPUT;
 	bool is_output = flags & GPIO_OUTPUT;
 
-	/* Disconnect not supported */
 	if (!is_input && !is_output) {
 		return -ENOTSUP;
 	}
 
-	/* Open source not supported*/
 	if (flags & GPIO_OPEN_SOURCE) {
 		return -ENOTSUP;
 	}
 
-	/* Pull up & pull down not supported in output mode */
 	if (is_output && (flags & (GPIO_PULL_UP | GPIO_PULL_DOWN)) != 0) {
 		return -ENOTSUP;
 	}
@@ -154,18 +148,12 @@ static int gpio_tc4x_port_toggle_bits(const struct device *dev, gpio_port_pins_t
 	return 0;
 }
 
-/**
- * @brief Configure pin or port
- */
 static int gpio_tc4x_config(const struct device *dev, gpio_pin_t pin, gpio_flags_t flags)
 {
 	const struct gpio_tc4x_config *cfg = dev->config;
 	int err;
 	Ifx_P_PADCFG_DRVCFG drvcfg;
 
-	/* figure out if we can map the requested GPIO
-	 * configuration
-	 */
 	err = gpio_tc4x_flags_to_drvcfg(flags, &drvcfg);
 	if (err != 0) {
 		return err;
@@ -193,9 +181,7 @@ static int gpio_tc4x_config(const struct device *dev, gpio_pin_t pin, gpio_flags
 }
 
 #if defined(CONFIG_GPIO_GET_CONFIG)
-/**
- * @brief Get configuration of pin
- */
+
 static int gpio_tc4x_get_config(const struct device *dev, gpio_pin_t pin, gpio_flags_t *flags)
 {
 	const struct gpio_tc4x_config *cfg = dev->config;
@@ -231,10 +217,48 @@ static int gpio_tc4x_pin_interrupt_configure(const struct device *dev, gpio_pin_
 	}
 
 	if (irq_src->type == TC4X_IRQ_TYPE_GTM) {
+		static bool egtm_initialized;
+		static uint8_t cls_clk_initialized;
+		volatile void *protse = (volatile void *)&MODULE_EGTM.PROTSE;
+
 		if (MODULE_EGTM.CLC.B.DISS == 1) {
 			if (!aurix_enable_clock((uintptr_t)&MODULE_EGTM.CLC, 1000)) {
 				return -EIO;
 			}
+		}
+
+		if (!egtm_initialized) {
+			if (!aurix_kernel_reset(
+				(uintptr_t)&MODULE_EGTM.RST.CTRLA, 1000)) {
+				return -EIO;
+			}
+			egtm_initialized = true;
+		}
+
+		aurix_prot_set_state(protse, AURIX_PROT_STATE_CONFIG);
+		
+		MODULE_EGTM.CLS[irq_src->cls].CCM.PROT.U = 0;
+
+		if ((cls_clk_initialized & BIT(irq_src->cls)) == 0) {
+			Ifx_EGTM_CLS_CMU_CLK_EN clk_en = {.U = 0};
+			
+			clk_en.B.EN_CLK0 = 2;
+			clk_en.B.EN_CLK1 = 2;
+			clk_en.B.EN_CLK2 = 2;
+			clk_en.B.EN_CLK3 = 2;
+			clk_en.B.EN_CLK4 = 2;
+			clk_en.B.EN_CLK5 = 2;
+			clk_en.B.EN_CLK6 = 2;
+			clk_en.B.EN_CLK7 = 2;
+			clk_en.B.EN_ECLK0 = 2;
+			clk_en.B.EN_ECLK1 = 2;
+			clk_en.B.EN_ECLK2 = 2;
+			clk_en.B.EN_FXCLK = 2;
+			MODULE_EGTM.CLS[irq_src->cls].CMU.CLK_EN = clk_en;
+			MODULE_EGTM.CLS[irq_src->cls].CMU.GCLK_NUM.U = 1;
+			MODULE_EGTM.CLS[irq_src->cls].CMU.GCLK_DEN.U = 1;
+
+			cls_clk_initialized |= BIT(irq_src->cls);
 		}
 
 		Ifx_EGTM_CLS_TIM_CH_CTRL ctrl = {.U = 0};
@@ -258,6 +282,9 @@ static int gpio_tc4x_pin_interrupt_configure(const struct device *dev, gpio_pin_
 		MODULE_EGTM.CLS[irq_src->cls].TIM.CH[irq_src->ch].FLT_RE.U = 0x5;
 		MODULE_EGTM.CLS[irq_src->cls].TIM.CH[irq_src->ch].IRQ_EN = irq_en;
 
+		MODULE_EGTM.CLS[irq_src->cls].CCM.PROT.U = 1;
+		aurix_prot_set_state(protse, AURIX_PROT_STATE_RUN);
+
 		irq_enable((0x1E60 / 4 + irq_src->cls * 8 + irq_src->ch));
 
 		return 0;
@@ -277,9 +304,6 @@ static int gpio_tc4x_pin_interrupt_configure(const struct device *dev, gpio_pin_
 		eicr.B.LDEN = (mode == GPIO_INT_MODE_LEVEL);
 		MODULE_SCU.ERU.EICR[irq_src->ch] = eicr;
 
-		/* We use the masking of the irq via the INTFx to emulate a
-		 * level type irq behavior. The irq is retriggered from the ISR
-		 * as long as the level is present. */
 		igcr.U = mode == GPIO_INT_MODE_LEVEL ? BIT(irq_src->ch) : 0;
 		igcr.B.IGP = mode == GPIO_INT_MODE_EDGE ? 1 : 2;
 		MODULE_SCU.ERU.IGCR[irq_src->cls] = igcr;
@@ -302,7 +326,7 @@ static DEVICE_API(gpio, tc4x_gpio_driver_api) = {
 	.pin_configure = gpio_tc4x_config,
 #if defined(CONFIG_GPIO_GET_CONFIG)
 	.pin_get_config = gpio_tc4x_get_config,
-#endif /* CONFIG_GPIO_GET_CONFIG */
+#endif 
 	.port_get_raw = gpio_tc4x_port_get_raw,
 	.port_set_masked_raw = gpio_tc4x_port_set_masked_raw,
 	.port_set_bits_raw = gpio_tc4x_port_set_bits_raw,
@@ -312,16 +336,6 @@ static DEVICE_API(gpio, tc4x_gpio_driver_api) = {
 	.manage_callback = gpio_tc4x_manage_callback,
 };
 
-/**
- * @brief Initialize GPIO port
- *
- * Perform basic initialization of a GPIO port. The code will
- * enable the clock for corresponding peripheral.
- *
- * @param dev GPIO device struct
- *
- * @return 0
- */
 static int gpio_tc4x_init(const struct device *dev)
 {
 	const struct gpio_tc4x_config *cfg = dev->config;
@@ -341,7 +355,7 @@ static int gpio_tc4x_init(const struct device *dev)
 		if (irq_src->type == TC4X_IRQ_TYPE_ERU) {                                         \
 			if (MODULE_SCU.ERU.EICR[irq_src->ch].B.LDEN) {                             \
 				if ((MODULE_SCU.ERU.EIFR.U & BIT(irq_src->ch)) != 0) {             \
-					/* Retrigger ERU IRQ for level type irqs */                \
+					                \
 					MODULE_SCU.ERU.FMR.U = BIT(irq_src->ch);                   \
 				}                                                                  \
 			} else {                                                                   \
