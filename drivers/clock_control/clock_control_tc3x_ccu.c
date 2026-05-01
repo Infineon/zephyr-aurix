c4cae7b07c4f176dd21b816eed4c65205c3235f1
Parthiban Nallathambi <parthiban@linumiz.com>
2026-05-02 01:16:21 +0530

    samples: gpio: button_interrupt: add tc397 ADAS kit overlay
    Sample needs sw0 alias plus a TIM IRQ source on the ADAS kit;
    the kit has no on-board user button so the overlay points to
    a header pin.
    
    Signed-off-by: Parthiban Nallathambi <parthiban@linumiz.com>

--- drivers/clock_control/clock_control_tc3x_ccu.c
+++ drivers/clock_control/clock_control_tc3x_ccu.c
@@ -96,6 +96,7 @@ __maybe_unused static inline int clock_control_tc3x_ccu_pll_wait_power_up()
 	Ifx_SCU_PERPLLSTAT perpllstat;
 	Ifx_SCU_SYSPLLSTAT syspllstat;
 
+	/* Wait 1ms to stabilize the pll */
 	uint64_t start_time = k_ticks_to_us_floor64(k_uptime_ticks());
 
 	if (!WAIT_FOR((perpllstat = MODULE_SCU.PERPLLSTAT, syspllstat = MODULE_SCU.SYSPLLSTAT,
@@ -181,6 +182,7 @@ static inline int clock_control_tc3x_ccu_select_clock(uint8_t clksel)
 	MODULE_SCU.CCUCON0 = ccucon0;
 	aurix_safety_endinit_enable(true);
 
+	/* Wait for new clock config to be active */
 	if (clksel) {
 		WAIT_FOR_CCUCON_UNLOCKED_OR_ERR(0, fpll_div3)
 	} else {
@@ -214,7 +216,7 @@ static inline int clock_control_tc3x_ccu_set_divider()
 		.B.MSCDIV = 0,
 		.B.QSPIDIV = CLOCK_DIV_WITH_INST(fqspi, infineon_qspi),
 		.B.I2CDIV = CLOCK_DIV_WITH_INST(fi2c, infineon_aurix_i2c),
-		.B.PLL1DIVDIS = 1, 
+		.B.PLL1DIVDIS = 1, // TODO: Implement pll1 div
 	};
 	Ifx_SCU_CCUCON2 ccucon2 = {
 		.B.CLKSELASCLINS =
@@ -348,6 +350,7 @@ static int clock_control_tc3x_ccu_init(const struct device *dev)
 
 #if !IS_ENABLED(CONFIG_SOC_TC3X_SLAVE_CORE)
 
+	/* Enable osc if requrired */
 	if (CLOCK_IS_CLOCK_SOURCE(fosc, FOSC_SOURCES)) {
 		if (!clock_control_tc3x_ccu_osc_ok()) {
 			ret = clock_control_tc3x_ccu_osc_init();
@@ -365,6 +368,7 @@ static int clock_control_tc3x_ccu_init(const struct device *dev)
 			return ret;
 		}
 
+		/* Configure pll parameters*/
 		clock_control_tc3x_ccu_pll_init();
 		if (ret) {
 			return ret;
@@ -380,7 +384,7 @@ static int clock_control_tc3x_ccu_init(const struct device *dev)
 			return ret;
 		}
 	} else {
-		
+		/* Disable pll power */
 		clock_control_tc3x_ccu_pll_power_down();
 
 		if (ret) {
@@ -393,6 +397,7 @@ static int clock_control_tc3x_ccu_init(const struct device *dev)
 		return -EIO;
 	}
 
+	/* Select clock for operation */
 	ret = clock_control_tc3x_ccu_select_clock(
 		DT_SAME_NODE(DT_CLOCKS_CTLR(DT_NODELABEL(fsource0)), DT_NODELABEL(fpll0)));
 	if (ret) {
@@ -400,7 +405,7 @@ static int clock_control_tc3x_ccu_init(const struct device *dev)
 	}
 
 	if (CLOCK_IS_CLOCK_SOURCE(fpll0, fsource0)) {
-		
+		/* Frequency stepping, to avoid load jumps */
 		ret = clock_control_tc3x_ccu_pll_set_devider();
 		if (ret) {
 			return ret;
