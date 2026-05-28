22c720e85872eeefca83b5d7a01372603e60c912
Parthiban Nallathambi <parthiban@linumiz.com>
2026-05-28 23:05:59 +0530

    drivers: interrupt_controller: consolidate AURIX IR driver
    Fold the AURIX interrupt-router driver into a single source file
    matching the upstream PR layout. Drop the private header and update
    the DT binding to the consolidated form.
    
    Signed-off-by: Parthiban Nallathambi <parthiban@linumiz.com>

--- drivers/interrupt_controller/intc_aurix_ir.c
+++ drivers/interrupt_controller/intc_aurix_ir.c
@@ -6,20 +6,27 @@
 
 #include <zephyr/arch/cpu.h>
 #include <zephyr/device.h>
+#include <zephyr/devicetree.h>
 #include <zephyr/drivers/interrupt_controller/intc_aurix_ir.h>
-#include "intc_aurix_ir_priv.h"
+
+#include <IfxInt_reg.h>
+#include <IfxSrc_regdef.h>
+#include <soc.h>
 
 #define DT_DRV_COMPAT infineon_aurix_ir
 
+#define GET_SRC(irq) (SRC_BASE + (irq) * 4)
+
 void intc_aurix_ir_irq_config(unsigned int irq, unsigned int priority, unsigned int flags)
 {
-	SRCR reg = {
+	Ifx_SRC_SRCR reg = {
 		.B.SRPN = priority,
-		.B.TOS = (flags & IRQ_USE_TOS) ? FIELD_GET(IRQ_TOS, flags) : CONFIG_AURIX_CORE_TOS,
+		.B.TOS = (flags & IRQ_USE_TOS) ? FIELD_GET(IRQ_TOS, flags)
+					       : aurix_coreid_to_tos(arch_proc_id()),
 		.B.CLRR = 1,
 		.B.IOVCLR = 1,
 #if IS_ENABLED(CONFIG_SOC_SERIES_TC4X)
-		.B.CS = CONFIG_TRICORE_CORE_ID == 6 ? 1 : 0,
+		.B.CS = IS_ENABLED(CONFIG_SOC_TC4X_CPUCS) ? 1 : 0,
 #endif
 #if IS_ENABLED(CONFIG_SOC_SERIES_TC3X)
 		.B.SWSCLR = 1,
@@ -27,35 +34,41 @@ void intc_aurix_ir_irq_config(unsigned int irq, unsigned int priority, unsigned 
 	};
 	sys_write32(reg.U, GET_SRC(irq));
 }
+
 void intc_aurix_ir_irq_enable(unsigned int irq)
 {
-	SRCR reg = {.U = sys_read32(GET_SRC(irq))};
+	Ifx_SRC_SRCR reg = {.U = sys_read32(GET_SRC(irq))};
+
 	reg.B.SRE = 1;
 	sys_write32(reg.U, GET_SRC(irq));
 }
 
 void intc_aurix_ir_irq_disable(unsigned int irq)
 {
-	SRCR reg = {.U = sys_read32(GET_SRC(irq))};
+	Ifx_SRC_SRCR reg = {.U = sys_read32(GET_SRC(irq))};
+
 	reg.B.SRE = 0;
 	sys_write32(reg.U, GET_SRC(irq));
 }
 
 bool intc_aurix_ir_irq_is_enabled(unsigned int irq)
 {
-	SRCR reg = {.U = sys_read32(GET_SRC(irq))};
+	Ifx_SRC_SRCR reg = {.U = sys_read32(GET_SRC(irq))};
+
 	return (reg.B.SRE == 1);
 }
 
 bool intc_aurix_ir_irq_is_pending(unsigned int irq)
 {
-	SRCR reg = {.U = sys_read32(GET_SRC(irq))};
+	Ifx_SRC_SRCR reg = {.U = sys_read32(GET_SRC(irq))};
+
 	return (reg.B.SRR == 1);
 }
 
 void intc_aurix_ir_irq_clear_pending(unsigned int irq)
 {
-	SRCR reg = {.U = sys_read32(GET_SRC(irq))};
+	Ifx_SRC_SRCR reg = {.U = sys_read32(GET_SRC(irq))};
+
 	reg.B.CLRR = 1;
 	sys_write32(reg.U, GET_SRC(irq));
 }
@@ -63,24 +76,24 @@ void intc_aurix_ir_irq_clear_pending(unsigned int irq)
 unsigned int intc_aurix_ir_get_active(void)
 {
 #if defined(CONFIG_SOC_SERIES_TC4X)
-	uint32_t lasr = sys_read32(IR_BASE + 0x0C20 + GET_TOS(arch_proc_id()) * 0x34);
-	return ((lasr >> 16) & 0x7FF);
+	return MODULE_INT.ICU[aurix_coreid_to_tos(arch_proc_id())].LASR.B.ID;
 #else
-	uint32_t lasr = sys_read32(IR_BASE + 0x204 + GET_TOS(arch_proc_id()) * 0x10);
-	return ((lasr >> 16) & 0x3FF);
+	return MODULE_INT.CH[aurix_coreid_to_tos(arch_proc_id())].LASR.B.ID;
 #endif
 }
 
 void intc_aurix_ir_irq_raise(unsigned int irq)
 {
-	SRCR reg = {.U = sys_read32(GET_SRC(irq))};
+	Ifx_SRC_SRCR reg = {.U = sys_read32(GET_SRC(irq))};
+
 	reg.B.SETR = 1;
 	sys_write32(reg.U, GET_SRC(irq));
 }
 
 void intc_aurix_ir_src_raise(uintptr_t src)
 {
-	SRCR reg = {.U = sys_read32(src)};
+	Ifx_SRC_SRCR reg = {.U = sys_read32(src)};
+
 	reg.B.SETR = 1;
 	sys_write32(reg.U, src);
 }
