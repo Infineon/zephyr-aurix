89a4397364fa162dbac029af81d3ba5b9ac1893d
Parthiban Nallathambi <parthiban@linumiz.com>
2026-05-29 17:29:04 +0530

    drivers: ethernet: dwc_ether_qos: assign a MAC when none is set
    The driver left mac_addr as all zeros, so the interface came up with
    00:00:00:00:00:00. DHCP could not address the node and incoming IPv4
    frames were dropped. Generate a locally administered random MAC at
    interface init when the address is unset.
    
    Signed-off-by: Parthiban Nallathambi <parthiban@linumiz.com>

--- drivers/ethernet/dwc_ether_qos/eth_qos.c
+++ drivers/ethernet/dwc_ether_qos/eth_qos.c
@@ -21,6 +21,7 @@ LOG_MODULE_REGISTER(eth_qos, CONFIG_ETHERNET_LOG_LEVEL);
 #include <zephyr/sys/barrier.h>
 #include <zephyr/sys/sys_io.h>
 #include <zephyr/sys/util.h>
+#include <zephyr/random/random.h>
 
 #include <ethernet/eth_stats.h>
 #include "eth_qos_reg.h"
@@ -622,6 +623,13 @@ void eth_qos_iface_init(struct net_if *iface)
 
 	data->iface = iface;
 	data->started = false;
+
+	if ((data->mac_addr[0] | data->mac_addr[1] | data->mac_addr[2] | data->mac_addr[3] |
+	     data->mac_addr[4] | data->mac_addr[5]) == 0U) {
+		sys_rand_get(data->mac_addr, sizeof(data->mac_addr));
+		data->mac_addr[0] |= 0x02U;
+		data->mac_addr[0] &= (uint8_t)~0x01U;
+	}
 
 	net_if_set_link_addr(iface, data->mac_addr, sizeof(data->mac_addr), NET_LINK_ETHERNET);
 	if (!cfg->dma_only) {
